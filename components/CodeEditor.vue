6a29b1fb5ba0f70431816b7546fa13c2a2b60a25
valentinoli <valentin.loftsson@gmail.com>
2021-07-22 13:26:26 +0200

    Change layout, various improvements

--- components/CodeEditor.vue
+++ components/CodeEditor.vue
@@ -13,8 +13,6 @@
 </template>
 
 <script>
-import { isError } from '@/lib/utils'
-
 // import Prism Editor
 import { PrismEditor } from 'vue-prism-editor'
 import 'vue-prism-editor/dist/prismeditor.min.css' // import the styles somewhere
@@ -32,9 +30,17 @@ import 'prismjs/components/prism-json'
 // import syntax highlighting styles
 import 'prismjs/themes/prism-tomorrow.css'
 
-function processValue(v) {
-  if (isError(v)) {
-    return v.stack || v.toString()
+function processValue(v, isError) {
+  if (isError) {
+    if (v.stack) {
+      return v.stack
+    }
+
+    if (typeof v === 'object') {
+      return Object.entries(v)
+        .reduce((acc, [k, v]) => `${acc}${k}: ${v}\n`, 'ERROR\n')
+        .trimRight()
+    }
   }
 
   return v
@@ -49,6 +55,10 @@ export default {
       type: [Object, String],
       default: ''
     },
+    error: {
+      type: Boolean,
+      default: false
+    },
     language: {
       type: String,
       default: 'text'
@@ -60,18 +70,18 @@ export default {
   },
   data() {
     return {
-      code: processValue(this.value)
+      code: processValue(this.value, this.error)
     }
   },
   computed: {
     editorLanguage() {
       // set editor language to 'text' if an error is being displayed
-      return isError(this.value) ? 'text' : this.language
+      return this.error ? 'text' : this.language
     }
   },
   watch: {
     value(v) {
-      this.code = processValue(v)
+      this.code = processValue(v, this.error)
     },
     code(v) {
       // propagate to parent component
--- components/DataExperience.vue
+++ components/DataExperience.vue
@@ -1,78 +1,76 @@
 <template>
   <div>
     <h1>{{ meta.title }}: {{ meta.subtitle }}</h1>
-    <h2 class="mt-3">Input</h2>
     <div class="io-block">
       <div class="mr-lg-6 mb-6">
-        <h3>File</h3>
-        <v-file-input
-          :label="label"
-          show-size
-          :accept="accept"
-          :multiple="multiple"
-          :loading="loading"
-          hide-details
-          @change="onFileChange"
-        >
-          <template v-if="inputFiles" #append>
-            <v-icon v-if="isError(inputFiles)" color="red">
-              mdi-cross
-            </v-icon>
-            <v-icon v-else color="green">
-              mdi-check
-            </v-icon>
-          </template>
-        </v-file-input>
-        <span v-if="uploadTime">{{ uploadTime }} sec.</span>
-        <code-editor
-          :value="inputFilesResult"
-          class="mt-9"
-          readonly
-          language="json"
-        />
-        <template v-if="showExtractedInputFiles">
-          <p class="font-weight-bold">Extracted files:</p>
-          <ul>
-            <li v-for="f in files" :key="f">{{ f }}</li>
-          </ul>
-        </template>
-      </div>
-      <div>
-        <h3 class="my-3">YARRRML</h3>
+        <h2 class="my-3">YARRRML</h2>
         <base-button @click="yarrrml = yarrrmlPrecontructed">
           Use preconstructed
         </base-button>
-        <base-button :disabled="parseYARRRMLDisabled" @click="parseYARRRML">
+        <base-button @click="generateRML">
           Generate RML
           <template v-if="rml">
-            <v-icon v-if="isError(rml)" right color="red">mdi-alert</v-icon>
+            <v-icon v-if="rmlError" right color="red">mdi-alert</v-icon>
             <v-icon v-else right color="green">mdi-check-circle</v-icon>
           </template>
         </base-button>
-        <code-editor :value.sync="yarrrml" language="yaml" />
+        <code-editor :value.sync="yarrrml" class="mt-6" language="yaml" />
       </div>
-    </div>
-    <h2>Output</h2>
-    <div class="io-block">
-      <div class="mr-lg-6">
-        <h3 class="my-3">RML</h3>
+      <div>
+        <h2 class="my-3">RML</h2>
         <base-button nuxt download :href="rmlHref" :disabled="!rml">
           <v-icon left>
             mdi-download
           </v-icon>
           Download
         </base-button>
+        <code-editor
+          :value="rml"
+          :error="rmlError"
+          class="mt-6"
+          readonly
+          language="turtle"
+        />
+      </div>
+    </div>
+    <div class="io-block">
+      <div class="mr-lg-6">
+        <h2>File</h2>
+        <v-file-input
+          :label="label"
+          show-size
+          :accept="accept"
+          :multiple="multiple"
+          :loading-files="filesLoading"
+          hide-details
+          @change="onFileChange"
+        >
+          <template v-if="!objectIsEmpty(inputFiles)" #append>
+            <v-icon v-if="filesError" color="red">
+              mdi-cross
+            </v-icon>
+            <v-icon v-else color="green">
+              mdi-check-circle
+            </v-icon>
+          </template>
+        </v-file-input>
+        <span v-if="filesUploadTime">{{ filesUploadTime }} sec.</span>
+        <code-editor
+          :value="inputFilesResult"
+          :error="filesError"
+          class="mt-9"
+          readonly
+        />
+      </div>
+      <div>
+        <h2 class="my-3">RDF</h2>
         <base-button :disabled="generateRDFDisabled" @click="generateRDF">
           Generate RDF
           <template v-if="rdf">
-            <v-icon v-if="isError(rdf)" right color="red">mdi-alert</v-icon>
+            <v-icon v-if="rdfError" right color="red">mdi-alert</v-icon>
             <v-icon v-else right color="green">mdi-check-circle</v-icon>
           </template>
         </base-button>
-        <code-editor :value="rml" class="mt-6" readonly language="turtle" />
-      </div>
-      <div>
-        <h3 class="my-3">RDF</h3>
         <base-button nuxt download :href="rdfHref" :disabled="!rdf">
           <v-icon left>
             mdi-download
@@ -80,7 +78,13 @@
           Download
         </base-button>
 
-        <code-editor :value="rdf" class="mt-6" readonly language="turtle" />
+        <code-editor
+          :value="rdf"
+          :error="rdfError"
+          class="mt-6"
+          readonly
+          language="turtle"
+        />
       </div>
     </div>
   </div>
@@ -92,7 +96,7 @@ import mapToRDF from '@/lib/map-to-rdf'
 import unzipit from '@/lib/unzip'
 import parseYARRRML from '@/lib/parse-yarrrml'
 
-import { isError, createObjectURL } from '@/lib/utils'
+import { createObjectURL, objectIsEmpty } from '@/lib/utils'
 
 export default {
   props: {
@@ -133,14 +137,17 @@ export default {
   },
   data() {
     return {
-      inputFiles: '',
-      loading: false,
-      uploadTime: 0,
+      inputFiles: {},
+      filesLoading: false,
+      filesError: false,
+      filesUploadTime: 0,
       yarrrmlPrecontructed: '',
       yarrrml: 'mappings:',
       rml: '',
+      rmlError: false,
       rmlHref: '',
       rdf: '',
+      rdfError: false,
       rdfHref: ''
     }
   },
@@ -157,28 +164,24 @@ export default {
     label() {
       return `Select file${this.multiple ? 's' : ''} (${this.ext})`
     },
-    parseYARRRMLDisabled() {
-      return !this.inputFiles || isError(this.inputFiles)
-    },
     generateRDFDisabled() {
-      return !this.rml || isError(this.rml)
+      return !this.rml || this.rmlError || objectIsEmpty(this.inputFiles)
     },
     inputFilesResult() {
-      const { inputFiles: f } = this
-      if (!f) {
-        return ''
-      }
-      if (isError(f)) {
+      const { inputFiles: f, files } = this
+      if (this.filesError) {
         return f
       }
-      const res = Object.fromEntries(
-        Object.entries(f).map(([k, v]) => [k, JSON.parse(v)])
-      )
-      return JSON.stringify(res, null, '\t')
-    },
-    showExtractedInputFiles() {
-      const { files: f, inputFilesResult: res } = this
-      return f.length && res && !isError(res)
+
+      if (objectIsEmpty(f)) {
+        return ''
+      }
+
+      if (files.length) {
+        return `Success!\nFiles extracted:\n${files.join('\n')}`
+      }
+
+      return 'Success!'
     }
   },
   async mounted() {
@@ -186,11 +189,12 @@ export default {
     this.yarrrmlPrecontructed = await result.text()
   },
   methods: {
-    isError,
+    objectIsEmpty,
     async onFileChange(submittedFiles) {
       try {
-        // set loading state while processing uploaded files
-        this.loading = true
+        // set filesLoading state while processing uploaded files
+        this.filesError = false
+        this.filesLoading = true
         const start = new Date()
 
         // read file/archive(s) asynchronously
@@ -206,7 +210,7 @@ export default {
         )
 
         // compute upload time
-        this.uploadTime = parseInt((new Date() - start) / 1000)
+        this.filesUploadTime = parseInt((new Date() - start) / 1000)
 
         // create inputFiles object with path-text pairs
         let inputFiles = {}
@@ -224,27 +228,33 @@ export default {
         this.inputFiles = inputFiles
       } catch (error) {
         console.error(error)
+        this.filesError = true
         this.inputFiles = error
       } finally {
-        this.loading = false
+        this.filesLoading = false
       }
     },
-    async parseYARRRML() {
+    async generateRML() {
       try {
+        this.rdfError = false
+        this.rmlError = false
         this.rml = await parseYARRRML(this.yarrrml)
         this.rmlHref = createObjectURL(this.rml, 'text/turtle')
       } catch (error) {
         console.error(error)
+        this.rmlError = true
         this.rml = error
         this.rmlHref = createObjectURL(this.rml)
       }
     },
     async generateRDF() {
       try {
+        this.rdfError = false
         this.rdf = await mapToRDF(this.rml, this.inputFiles, this.functions)
         this.rdfHref = createObjectURL(this.rdf, 'text/n3; charset=utf-8')
       } catch (error) {
         console.error(error)
+        this.rdfError = true
         this.rdf = error
         this.rdfHref = createObjectURL(this.rdf)
       }
