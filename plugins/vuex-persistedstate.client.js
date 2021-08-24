7b37f6767564b13d150e9dc734e728d656aeac7d
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2021-08-24 15:06:43 +0200

    Remove unused code

--- plugins/vuex-persistedstate.client.js
+++ plugins/vuex-persistedstate.client.js
@@ -4,23 +4,5 @@ import createPersistedState from 'vuex-persistedstate'
 export default ({ store, req }) => {
   createPersistedState({
     paths: ['power']
-    // storage: {
-    //   getItem: key => {
-    //     if (process.server) {
-    //       // Node.js server
-    //       if (!req.headers.cookie) {
-    //         return null
-    //       }
-    //       const parsedCookies = cookie.parse(req.headers.cookie)
-    //       return parsedCookies[key]
-    //     } else {
-    //       return Cookies.get(key)
-    //     }
-    //   },
-    //   // https://github.com/js-cookie/js-cookie/blob/master/SERVER_SIDE.md#express
-    //   setItem: (key, value) =>
-    //     Cookies.set(key, value, { expires: 365, secure: false }),
-    //   removeItem: key => Cookies.remove(key)
-    // }
   })(store)
 }
