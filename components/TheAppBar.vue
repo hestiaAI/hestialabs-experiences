66db5c4e338cc21233e8f9b2f12e3858090b54e6
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2021-08-24 13:56:45 +0200

    Isolate the app bar

---
+++ components/TheAppBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <v-app-bar fixed app color="white" height="75" style="z-index: 2000;">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>
+        <nuxt-link
+          to="/"
+          class="d-flex align-center"
+          style="text-decoration: none; color: inherit;"
+        >
+          <logo-img class="mr-5" width="100" />
+        </nuxt-link>
+      </v-toolbar-title>
+      <v-spacer />
+      <mode-switch v-if="$vuetify.breakpoint.smAndUp" />
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      temporary
+      class="pa-5"
+      :width="500"
+      style="z-index: 3000;"
+    >
+      <template #prepend>
+        <div class="d-flex justify-space-between align-center">
+          <v-btn icon to="/"><v-icon>mdi-home-outline</v-icon></v-btn>
+          <v-btn icon @click="drawer = false"><v-icon>mdi-close</v-icon></v-btn>
+        </div>
+      </template>
+      <template #append>
+        <mode-switch />
+      </template>
+      <div class="mt-6">
+        <logo-img width="250" />
+        <the-data-experience-list class="mt-6" />
+      </div>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false
+    }
+  }
+}
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,41 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar fixed app color="white" height="75" style="z-index: 2000;">
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>
-        <nuxt-link
-          to="/"
-          class="d-flex align-center"
-          style="text-decoration: none; color: inherit;"
-        >
-          <logo-img class="mr-5" width="100" />
-        </nuxt-link>
-      </v-toolbar-title>
-      <v-spacer />
-      <mode-switch v-if="$vuetify.breakpoint.smAndUp" />
-    </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      temporary
-      class="pa-5"
-      :width="500"
-      style="z-index: 3000;"
-    >
-      <template #prepend>
-        <div class="d-flex justify-space-between align-center">
-          <v-btn icon to="/"><v-icon>mdi-home-outline</v-icon></v-btn>
-          <v-btn icon @click="drawer = false"><v-icon>mdi-close</v-icon></v-btn>
-        </div>
-      </template>
-      <template #append>
-        <mode-switch />
-      </template>
-      <div class="mt-6">
-        <logo-img width="250" />
-        <the-data-experience-list class="mt-6" />
-      </div>
-    </v-navigation-drawer>
+    <the-app-bar />
     <v-main>
       <v-container class="mt-5">
         <nuxt />
@@ -69,7 +34,6 @@
 export default {
   data() {
     return {
-      drawer: false,
       // Display offline message if user opens app when offline
       snackbar: this.$nuxt.isOffline,
       timeout: 5000
