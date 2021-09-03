338726f071271773c77cbe9ec8a75454f58c5d2f
Florian Singer <fsinger@mailbox.org>
2021-09-03 11:15:47 +0200

    Fix update requests and clean up code

--- components/BaseShareButton.vue
+++ components/BaseShareButton.vue
@@ -7,18 +7,7 @@
 </template>
 
 <script>
-import {
-  // eslint-disable-next-line no-unused-vars
-  updateEndpoint,
-  // eslint-disable-next-line no-unused-vars
-  updateEndpointWhere,
-  // eslint-disable-next-line no-unused-vars
-  queryEndpoint
-} from '~/utils/endpoint'
-// updateEndpoint(
-//   'INSERT DATA { <http://schema.org/Organization/%40Apple> <http://schema.org/name> "Apple" . }'
-// )
-// queryEndpoint('SELECT * WHERE { ?s ?p ?o } LIMIT 10')
+import { updateEndpoint } from '~/utils/endpoint'
 
 export default {
   props: {
@@ -38,10 +27,6 @@ export default {
   methods: {
     async share() {
       await updateEndpoint(this.data)
-      // await updateEndpointWhere(
-      //   'INSERT { ?s ?p ?o } WHERE { ?s ?p ?o }',
-      //   this.data
-      // )
     }
   }
 }
--- utils/endpoint.js
+++ utils/endpoint.js
@@ -1,47 +1,32 @@
-// import { SparqlEndpointFetcher } from 'fetch-sparql-endpoint'
 import { newEngine } from '@comunica/actor-init-sparql'
-import { Store, Parser } from 'n3'
 
-// const fetcher = new SparqlEndpointFetcher()
 const engine = newEngine()
 
+// TODO put in a separate config file
 const endpoint = 'http://localhost:3001/sparql'
 
 export async function updateEndpoint(rdf) {
-  console.log('Sending data to ' + endpoint)
-
   const sparql = 'INSERT DATA { ' + rdf + ' }'
-  const result = await engine.query(sparql, {
-    destination: { type: 'sparql', value: endpoint }
+
+  const httpResponse = await fetch(endpoint, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/sparql-update'
+    },
+    body: sparql
   })
-  await result.updateResult
-  // await fetcher.fetchUpdate(endpoint, sparql)
-  console.log('Data sent')
-}
-
-export async function updateEndpointWhere(sparql, sourceRdf) {
-  console.log('Sending data to ' + endpoint)
-
-  const parser = new Parser({ format: 'N3' })
-  const quads = parser.parse(sourceRdf)
-  const store = new Store(quads)
-
-  const result = await engine.query(sparql, {
-    sources: [store],
-    destination: { type: 'sparql', value: endpoint }
-  })
-  await result.updateResult
-  // store.forEach(console.log, null, null, null, null)
-  console.log('Data sent')
+  if (!httpResponse.ok) {
+    throw new Error(
+      `Invalid SPARQL endpoint response from ${endpoint} (HTTP status ${httpResponse.status}):\n`
+    )
+  }
 }
 
 export async function queryEndpoint(sparql) {
-  console.log('Receiving data from ' + endpoint)
-
   const result = await engine.query(sparql, {
     sources: [endpoint]
   })
   const bindings = await result.bindings()
 
-  console.log(bindings)
+  return bindings
 }
