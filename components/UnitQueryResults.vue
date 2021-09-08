86ebb66f4fcc33bfe3c99a78dc4b3e38406a048e
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2021-09-08 21:58:24 +0200

    Fix query results download button bug

--- components/UnitQueryResults.vue
+++ components/UnitQueryResults.vue
@@ -3,14 +3,12 @@
     <h2 class="my-3">Query Results</h2>
     <base-download-button
       v-bind="{ progress, error, disabled, extension, data, status }"
+      ref="downloadButton"
       text="Download"
       @click.native="onDownload"
     />
     <v-alert v-if="error" type="error">{{ message }}</v-alert>
-    <the-query-results-data-table
-      v-bind="{ headers, items }"
-      :hide-default-footer="disabled"
-    />
+    <v-data-table v-bind="{ headers, items }" :hide-default-footer="disabled" />
   </div>
 </template>
 
@@ -45,20 +43,32 @@ export default {
     }
   },
   methods: {
-    async onDownload() {
-      this.progress = true
-      this.status = false
-      this.error = false
-      try {
-        const headers = this.headers.map(h => h.text)
-        this.data = await writeToString(this.items, { headers })
-      } catch (error) {
-        console.error(error)
-        this.error = true
-        this.message = processError(error)
-      } finally {
-        this.progress = false
-        this.status = true
+    async onDownload(event) {
+      // only if the event is trusted (i.e. triggered by user interaction)
+      if (event.isTrusted) {
+        // prevent default anchor behavior,
+        // i.e. prevent triggering the download
+        // since we want to generate the file contents first
+        event.preventDefault()
+        this.progress = true
+        this.status = false
+        this.error = false
+        try {
+          const headers = this.headers.map(h => h.text)
+          // update the data
+          this.data = await writeToString(this.items, { headers })
+          // wait until DOM is updated, i.e. the href attribute (see BaseDownloadButton.vue)
+          await this.$nextTick()
+          // click the anchor manually -> event.isTrusted === false
+          this.$refs.downloadButton.$el.click()
+        } catch (error) {
+          console.error(error)
+          this.error = true
+          this.message = processError(error)
+        } finally {
+          this.progress = false
+          this.status = true
+        }
       }
     }
   }
