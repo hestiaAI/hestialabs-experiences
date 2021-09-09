4285478b458ed7fdfa066c01b8516923b0818838
Florian Singer <fsinger@mailbox.org>
2021-09-09 17:29:35 +0200

    Refactor share button

--- components/UnitShareData.vue
+++ components/UnitShareData.vue
@@ -4,12 +4,24 @@
     <div class="d-flex flex-column flex-sm-row align-start">
       <slot name="selector" :change="v => (sparql = v)" class="ma-sm-2" />
       <base-button
-        v-bind="{ progress, status, error, disabled }"
+        :progress="progress[0]"
+        :status="status[0]"
+        :error="error[0]"
+        :disabled="disabledGenerate"
         text="Generate RDF"
         class="ma-sm-2"
         @click="constructRDF"
       />
-      <base-share-button :data="rdfOutput" class="ma-sm-2" />
+      <base-button
+        :progress="progress[1]"
+        :status="status[1]"
+        :error="error[1]"
+        :disabled="disabledShare"
+        text="Share"
+        icon="mdiShareVariant"
+        class="ma-sm-2"
+        @click="share"
+      />
       <base-download-button
         extension="nq"
         :data="rdfOutput"
@@ -29,6 +41,7 @@
 
 <script>
 import { construct } from '@/utils/sparql'
+import { updateEndpoint } from '~/utils/endpoint'
 
 export default {
   props: {
@@ -40,30 +53,48 @@ export default {
   data() {
     return {
       sparql: '',
-      status: false,
-      error: false,
-      progress: false,
+      progress: [false, false],
+      status: [false, false],
+      error: [false, false],
       rdfOutput: ''
     }
   },
   computed: {
-    disabled() {
-      return !this.rdfInput || !this.sparql || this.progress
+    disabledGenerate() {
+      const i = 0
+      return !this.rdfInput || !this.sparql || this.progress[i]
+    },
+    disabledShare() {
+      const i = 1
+      return !this.rdfOutput || this.progress[i]
     }
   },
   methods: {
     async constructRDF() {
-      this.progress = true
-      this.status = false
-      this.error = false
+      const i = 0
+      this.error[i] = false
+      this.progress[i] = true
       try {
         this.rdfOutput = await construct(this.rdfInput, this.sparql)
       } catch (error) {
         console.error(error)
-        this.error = true
+        this.error[i] = true
       }
-      this.progress = false
-      this.status = true
+      this.status[i] = true
+      this.progress[i] = false
+    },
+    async share() {
+      const i = 1
+      this.error[i] = false
+      this.progress[i] = true
+      try {
+        await updateEndpoint(this.rdfOutput)
+      } catch (error) {
+        console.error(error)
+        this.error[i] = true
+      }
+      this.status[i] = true
+      this.progress[i] = false
     }
   }
 }
