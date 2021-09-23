db6386748c933843a6f99a402d88c8a421f6d1f3
Florian Singer <fsinger@mailbox.org>
2021-09-23 15:31:50 +0200

    Add CSV processor for sunburst

--- manifests/csv-processors.js
+++ manifests/csv-processors.js
@@ -1,6 +1,76 @@
 const sunburstTargeting = (headers, items) => {
-  console.error('TODO implement')
-  return [headers, items]
+  // TODO generalize this...
+  const results = []
+  const tree = new Map()
+  // Construct tree: Root -> AdvertiserName -> TargetingType -> [TargetingValue, count]
+  for (const item of items) {
+    const advertiser = item.advertiserName
+    const targetingType = item.targetingType
+    const targetingValue = item.targetingValue
+    const count = parseInt(item.count)
+    if (!tree.has(advertiser)) {
+      tree.set(advertiser, new Map())
+    }
+    const subtree = tree.get(advertiser)
+    if (!subtree.has(targetingType)) {
+      subtree.set(targetingType, [])
+    }
+    const leaves = subtree.get(targetingType)
+    leaves.push([targetingValue, count])
+  }
+  // Flatten tree into CSV. Give IDs and propagate aggregated counts to parents
+  let id = 0
+  let rootValue = 0
+  for (const [advertiser, subtree] of tree.entries()) {
+    id += 1
+    const advertiserID = id
+    let advertiserValue = 0
+    for (const [targetingType, leaves] of subtree.entries()) {
+      id += 1
+      const targetingTypeID = id
+      let targetingTypeValue = 0
+      for (const [targetingValue, count] of leaves) {
+        id += 1
+        targetingTypeValue += count
+        results.push({
+          id,
+          name: targetingValue,
+          value: count,
+          group: advertiser,
+          parent: targetingTypeID,
+          size: count
+        })
+      }
+      advertiserValue += targetingTypeValue
+      results.push({
+        id: targetingTypeID,
+        name: targetingType,
+        value: targetingTypeValue,
+        group: advertiser,
+        parent: advertiserID,
+        size: null
+      })
+    }
+    rootValue += advertiserValue
+    results.push({
+      id: advertiserID,
+      name: advertiser,
+      value: advertiserValue,
+      group: advertiser,
+      parent: 0,
+      size: null
+    })
+  }
+  const root = {
+    id: 0,
+    name: 'All',
+    value: rootValue,
+    group: 'All',
+    parent: null,
+    size: null
+  }
+  results.push(root)
+  return [Object.keys(root), results]
 }
 
 export default {
