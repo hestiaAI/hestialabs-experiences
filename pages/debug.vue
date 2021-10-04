2984930fec31797b71e3a40fa20d0561b106d63e
andreaskundig <andreas.kuendig@gmx.ch>
2021-10-04 17:48:28 +0200

    another try at uploading file

--- pages/debug.vue
+++ pages/debug.vue
@@ -7,9 +7,10 @@
         <div>
           <form ref="form" name="file-upload" method="post" data-netlify="true">
             <div>
-              <input type="hidden" name="form-name" value="file-upload" />
+              <!-- <input type="hidden" name="form-name" value="file-upload" /> -->
               <div>
                 <input
+                  v-model="name"
                   for="name"
                   type="text"
                   name="name"
@@ -18,7 +19,7 @@
                 />
               </div>
               <div>
-                <input type="file" name="picture" />
+                <input type="file" name="picture" @change="handleFile" />
               </div>
               <div>
                 <button
@@ -39,10 +40,34 @@
 
 <script>
 export default {
+  data() {
+    return {
+      formName: 'file-upload',
+      name: '',
+      file: null
+    }
+  },
   methods: {
+    handleFile(event) {
+      const files = event.target.files
+      const filename = files[0].name
+      console.log('filename', filename)
+      // const fileReader = new FileReader()
+      // fileReader.addEventListener('load', () => {
+      //   this.imageUrl = fileReader.result
+      // })
+      // fileReader.readAsDataURL(files[0])
+      this.file = files[0]
+    },
     handleSubmit() {
-      const myForm = this.$refs.form
-      const formData = new FormData(myForm)
+      console.log(this.name, this.file, this.formName)
+      const formData = new FormData()
+      formData.append('name', this.name)
+      formData.append('form-name', this.formName)
+      formData.append('file', this.file)
+      // const myForm = this.$refs.form
+      // console.log(myForm)
+      // const formData = new FormData(myForm)
       fetch('/', {
         method: 'POST',
         headers: { 'Content-Type': 'multipart/form-data' },
