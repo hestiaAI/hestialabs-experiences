57497ba4f4a1b6005115454c1f12f88c4c9af0fd
andreaskundig <andreas.kuendig@gmx.ch>
2021-10-04 17:09:28 +0200

    upload file programmatically to netlify forms

--- pages/debug.vue
+++ pages/debug.vue
@@ -5,7 +5,7 @@
     <template>
       <section>
         <div>
-          <form name="file-upload" method="post" data-netlify="true">
+          <form ref="form" name="file-upload" method="post" data-netlify="true">
             <div>
               <input type="hidden" name="form-name" value="file-upload" />
               <div>
@@ -18,17 +18,16 @@
                 />
               </div>
               <div>
-                <textarea
-                  name="message"
-                  required
-                  placeholder="Message"
-                ></textarea>
-              </div>
-              <div>
                 <input type="file" name="picture" />
               </div>
               <div>
-                <button type="submit" value="Send file">Submit</button>
+                <button
+                  type="submit"
+                  value="Send file"
+                  @click.prevent="handleSubmit"
+                >
+                  Submit
+                </button>
               </div>
             </div>
           </form>
@@ -37,3 +36,21 @@
     </template>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    handleSubmit() {
+      const myForm = this.$refs.form
+      const formData = new FormData(myForm)
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'multipart/form-data' },
+        body: new URLSearchParams(formData).toString()
+      })
+        .then(() => console.log('Form successfully submitted'))
+        .catch(error => alert(error))
+    }
+  }
+}
+</script>
