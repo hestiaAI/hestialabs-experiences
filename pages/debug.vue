ac7334691ff80947344a022218bbd1ddafc7e1cb
andreaskundig <andreas.kuendig@gmx.ch>
2021-10-04 15:56:03 +0200

    add debug page to test netlify file upload

---
+++ pages/debug.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>How did you get here?</h1>
+    <p>This is just to test netlify forms file</p>
+    <template>
+      <section>
+        <div>
+          <form name="file-upload" method="post" data-netlify="true">
+            <div>
+              <input type="hidden" name="form-name" value="contact-form" />
+              <div>
+                <input
+                  for="name"
+                  type="text"
+                  name="name"
+                  required
+                  placeholder="Name"
+                />
+              </div>
+              <div>
+                <textarea
+                  name="message"
+                  required
+                  placeholder="Message"
+                ></textarea>
+              </div>
+              <div>
+                <input type="file" name="picture" />
+              </div>
+              <div>
+                <button type="submit" value="Send file">Submit</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </section>
+    </template>
+  </div>
+</template>
