b976ba5b514ee44eb8a1117831d6734e55a81a9e
francoisquellec <francois.quellec@gmail.com>
2021-11-02 11:25:06 +0100

    add properly leaflet for vuejs

--- components/graphs/GoogleMapOverview.vue
+++ components/graphs/GoogleMapOverview.vue
@@ -1,13 +1,10 @@
 <template>
   <v-container>
     <v-row>
-      <div id="holder">
-        <div id="demo1">
-          <div id="map"></div>
-          <div id="#cluster-map-anchor"></div>
-          <div id="pie"></div>
-        </div>
-      </div>
+      <l-map style="height: 300px" :zoom="zoom" :center="center">
+        <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+        <l-marker :lat-lng="markerLatLng"></l-marker>
+      </l-map>
     </v-row>
   </v-container>
 </template>
@@ -15,16 +12,27 @@
 <script>
 import * as d3 from 'd3'
 import * as dc from 'dc'
-import crossfilter from 'crossfilter2'
-import L from 'leaflet'
-// eslint-disable-next-line camelcase
-import dc_leaflet from 'dc.leaflet'
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+
+// Fix missing marker issue
+import { Icon } from 'leaflet'
+delete Icon.Default.prototype._getIconUrl
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+})
 
 // Remove warning on default colorscheme, even if not used..
 dc.config.defaultColors(d3.schemePaired)
 
 export default {
   name: 'GoogleMapOverview',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker
+  },
   props: {
     values: {
       type: Array,
@@ -42,7 +50,12 @@ export default {
         { text: 'daddr', value: 'daddr' },
         { text: 'Category', value: 'Category' }
       ],
-      results: []
+      results: [],
+      zoom: 7,
+      center: [-34.9205, -57.953646],
+      markerLatLng: [-34.9205, -57.953646],
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      attribution: ''
     }
   },
   mounted() {
@@ -55,51 +68,15 @@ export default {
         d.lat = +d.lat
         d.lon = +d.lon
       })
-      const clusterMap = L.map('map', {
-        center: [42.69, 25.42],
-        zoom: 7
-      })
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(clusterMap)
 
-      const ndx = crossfilter(this.results)
-      const groupname = 'marker-select'
-
-      const facilities = ndx.dimension(d => [d.lat, d.lon])
-      const facilitiesGroup = facilities.group().reduceCount()
-
-      dc_leaflet
-        .markerChart('#cluster-map-anchor', groupname)
-        .dimension(facilities)
-        .group(facilitiesGroup)
-        .map(clusterMap)
-
-      const types = ndx.dimension(function (d) {
-        return d.type
-      })
-      const typesGroup = types.group().reduceCount()
-
-      dc.pieChart('#pie', groupname)
-        .dimension(types)
-        .group(typesGroup)
-        .width(200)
-        .height(200)
-        .renderLabel(true)
-        .renderTitle(true)
-        .ordering(function (p) {
-          return -p.value
-        })
-
-      dc.renderAll(groupname)
+      dc.renderAll()
     }
   }
 }
 </script>
 <style>
 @import 'assets/styles/dc.css';
-
+@import 'leaflet/dist/leaflet.css';
 body {
   font-family: sans-serif;
   color: #22313f;
@@ -125,19 +102,5 @@ body {
 p.filters {
   font-size: 0.8rem;
   font-style: italic;
-}
-
-.map {
-  width: 600px;
-  height: 400px;
-}
-
-#holder {
-  width: 850px;
-  margin: 20px auto;
-}
-#holder > div {
-  padding: 30px 0;
-  clear: both;
 }
 </style>
