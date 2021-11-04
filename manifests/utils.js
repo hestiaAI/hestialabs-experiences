7d46fe61236cdd19aa38b68f3c2addc414afc2d8
Luã Streit <lua.streit@protonmail.com>
2021-11-04 17:41:00 +0100

    WIP

--- manifests/utils.js
+++ manifests/utils.js
@@ -5,3 +5,19 @@ export const validExtensions = ['csv', 'js', 'json', 'xml', 'zip']
 // helper function to extract the top directory in a path
 export const extractFirstDirectory = path =>
   path.match(/^(?:\.\/)?([a-z0-9-]+)\//)[1]
+
+/*
+function fileTreeToList(tree) {
+  return Object.entries(tree).map(([key, value]) => {
+    if (Array.isArray(value)) {
+      return { filename: key, file: value[0] }
+    } else {
+      const child = fileTreeToList()
+      return { filename: `${key}/${child.filename}`, file: child.file }
+    }
+  })
+}
+export function processFiles(fileTree, timedObservationsViewer) {
+  const fileNameList = fileTreeToList(fileTree)
+}
+*/
