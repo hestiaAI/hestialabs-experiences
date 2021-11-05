27d27a6173c3382339ca90d61cdaceebadce0e79
Florian Singer <fsinger@mailbox.org>
2021-11-05 09:43:07 +0100

    perf: replace sunburst sparql with custom pipeline

--- manifests/experiences/twitter/pipeline.js
+++ manifests/experiences/twitter/pipeline.js
@@ -1,5 +1,7 @@
 import { JSONPath } from 'jsonpath-plus'
+import _ from 'lodash'
 import { aggregate, countReducer } from '@/utils/aggregate'
+import csvProcessors from '@/manifests/csv-processors'
 
 function dashboardFillItems(items, impressionAttributes, isEngagement) {
   impressionAttributes.forEach(v => {
@@ -71,7 +73,61 @@ async function advertisersPerDay(inputFiles) {
   return await Promise.resolve({ headers, items })
 }
 
+async function targetingTree(inputFiles) {
+  // JSON iterator on impressions
+  const impressionsFile = JSON.parse(inputFiles['data/ad-impressions.js'])
+  const impressions = JSONPath({
+    path: '$.*.ad.adsUserData.adImpressions.impressions[*]',
+    json: impressionsFile
+  })
+  // Find 10 most present advertisers
+  const advertisers = impressions.map(v => ({
+    advertiserName: v.advertiserInfo.advertiserName
+  }))
+  const advertisersCount = aggregate(
+    advertisers,
+    ['advertiserName'],
+    countReducer('count')
+  )
+  const topAdvertisers = _.chain(advertisersCount)
+    .sortBy(x => -x.count)
+    .take(10)
+    .map('advertiserName')
+    .value()
+  // Iterator on targeting criteria
+  let items = []
+  impressions.forEach(v => {
+    // Filter top advertisers
+    if (topAdvertisers.includes(v.advertiserInfo.advertiserName)) {
+      const criteria = JSONPath({
+        path: '$.matchedTargetingCriteria[*]',
+        json: v
+      })
+      criteria.forEach(criterion => {
+        // Select relevant fields
+        items.push({
+          advertiserName: v.advertiserInfo.advertiserName,
+          targetingType: criterion.targetingType,
+          targetingValue: criterion.targetingValue
+        })
+      })
+    }
+  })
+  let headers = ['advertiserName', 'targetingType', 'targetingValue', 'count']
+  // GroupBy and count
+  items = aggregate(
+    items,
+    ['advertiserName', 'targetingType', 'targetingValue'],
+    countReducer('count')
+  )
+  // Transform to tree
+  ;[headers, items] = csvProcessors.sunburstTargeting(headers, items)
+
+  return await Promise.resolve({ headers, items })
+}
+
 export default {
   dashboard,
-  advertisersPerDay
+  advertisersPerDay,
+  targetingTree
 }
