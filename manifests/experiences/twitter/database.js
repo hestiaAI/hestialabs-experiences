14fab393c275b88c8394b9a14f9d4504fd95fc89
Florian Singer <fsinger@mailbox.org>
2021-11-15 18:03:03 +0100

    perf: sql pipeline for wordcloud

--- manifests/experiences/twitter/database.js
+++ manifests/experiences/twitter/database.js
@@ -11,6 +11,12 @@ export default async function databaseBuilder(fileManager) {
     ['time', 'DATE'],
     ['engagement', 'INTEGER']
   ])
+  db.create('twitterCriteria', [
+    ['id', 'INTEGER'],
+    ['adId', 'INTEGER'],
+    ['targetingType', 'TEXT'],
+    ['targetingValue', 'TEXT']
+  ])
 
   const impressionsFile = JSON.parse(
     await fileManager.getPreprocessedText('data/ad-impressions.js')
@@ -19,12 +25,30 @@ export default async function databaseBuilder(fileManager) {
     path: '$.*.ad.adsUserData.adImpressions.impressions[*]',
     json: impressionsFile
   })
-  const items = impressions.map((v, i) => ({
-    id: i,
-    tweetId: v.promotedTweetInfo?.tweetId ?? null,
-    advertiserName: v.advertiserInfo.advertiserName,
-    time: v.impressionTime,
-    engagement: 0
-  }))
-  db.insert('twitterAds', items)
+  const adsItems = []
+  const targetingItems = []
+  let j = 0
+  impressions.forEach((v, i) => {
+    adsItems.push({
+      id: i,
+      tweetId: v.promotedTweetInfo?.tweetId ?? null,
+      advertiserName: v.advertiserInfo.advertiserName,
+      time: v.impressionTime,
+      engagement: 0
+    })
+    const criteria = JSONPath({
+      path: '$.matchedTargetingCriteria[*]',
+      json: v
+    })
+    criteria.forEach(criterion => {
+      targetingItems.push({
+        id: j++,
+        adId: i,
+        targetingType: criterion.targetingType,
+        targetingValue: criterion?.targetingValue ?? null
+      })
+    })
+  })
+  db.insert('twitterAds', adsItems)
+  db.insert('twitterCriteria', targetingItems)
 }
