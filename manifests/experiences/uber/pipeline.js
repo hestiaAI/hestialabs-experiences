af55fd8f8128f41fbe2ec28baa04c9536b591a3d
andreaskundig <andreas.kuendig@gmx.ch>
2021-11-24 12:15:56 +0100

    merge uber-kepler into kepler experience

--- manifests/experiences/uber/pipeline.js
+++ manifests/experiences/uber/pipeline.js
@@ -4,6 +4,10 @@ async function tripsData(fileManager) {
   return await fileManager.getCsvItems('Uber Data/Rider/trips_data.csv')
 }
 
+async function rawTripsData(fileManager) {
+  return await fileManager.getText('Uber Data/Rider/trips_data.csv')
+}
+
 async function tripsGraphData(fileManager) {
   const tripsData = await fileManager.getCsvItems(
     'Uber Data/Rider/trips_data.csv'
@@ -28,6 +32,7 @@ async function tripsGraphData(fileManager) {
 }
 export default {
   tripsData,
+  rawTripsData,
   tripsGraphData,
   genericDateViewer
 }
