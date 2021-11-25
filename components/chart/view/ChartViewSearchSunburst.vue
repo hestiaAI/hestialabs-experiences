3302b3a3075b8bf7d9467b98b45df8c3176c305c
francoisquellec <francois.quellec@gmail.com>
2021-11-25 13:35:05 +0100

    init SearchSunburst with a clickable table

---
+++ components/chart/view/ChartViewSearchSunburst.vue
@@ -0,0 +1,61 @@
+<template>
+  <VContainer>
+    <VRow>
+      <VCol cols="5">
+        <VCard>
+          <VCardTitle>
+            Advertisers
+            <VSpacer></VSpacer>
+            <VTextField
+              v-model="search"
+              append-icon="mdi-magnify"
+              label="Search"
+              single-line
+              hide-details
+            ></VTextField>
+          </VCardTitle>
+          <VDataTable
+            v-model="selected"
+            :headers="headers"
+            :items="items"
+            :single-select="true"
+            :search="search"
+            item-key="name"
+            show-select
+            class="elevation-1"
+          >
+          </VDataTable>
+        </VCard>
+      </VCol>
+    </VRow>
+  </VContainer>
+</template>
+
+<script>
+import mixin from './mixin'
+
+export default {
+  mixins: [mixin],
+  data() {
+    return {
+      search: '',
+      selected: [{ name: 'Apple', value: 12 }],
+      headers: [
+        { text: 'Advertiser', value: 'name' },
+        { text: 'Impressions', value: 'value' }
+      ],
+      items: [
+        { name: 'Apple', value: 12 },
+        { name: 'Samsung', value: 11 },
+        { name: 'Android', value: 10 },
+        { name: 'Sony', value: 9 }
+      ]
+    }
+  },
+  methods: {
+    drawViz() {
+      console.log(this.values)
+    }
+  }
+}
+</script>
