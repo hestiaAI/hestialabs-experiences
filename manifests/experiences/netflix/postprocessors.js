21cd627b63689e29aca27dc46ef2d2474619f875
francoisquellec <francois.quellec@gmail.com>
2021-12-07 09:29:30 +0100

    remove unused postprocessor

--- manifests/experiences/netflix/postprocessors.js
+++ manifests/experiences/netflix/postprocessors.js
@@ -1,29 +1,3 @@
-const usagePerDay = result => {
-  const items = result?.items || []
-  if (items.length === 0) {
-    return { headers: [], items: [] }
-  }
-  const results = []
-  // get unique users
-  const users = items
-    .map(i => i['Profile Name'])
-    .filter((value, index, self) => self.indexOf(value) === index)
-  items.forEach(i => {
-    const time = i.Duration.split(':')
-    const hours = +time[0] + +time[1] / 60 + +time[2] / 60 / 60
-    const row = {
-      date: new Date(i['Start Time'])
-    }
-    users.forEach(u => {
-      if (i['Profile Name'] === u) row[u] = hours
-      else row[u] = 0
-    })
-
-    results.push(row)
-  })
-  results.sort((e1, e2) => e1.date - e2.date)
-  return { headers: ['date', users].flat(), items: results }
-}
 const notifsPerDay = result => {
   const items = result?.items || []
   if (items.length === 0) {
@@ -52,6 +26,5 @@ const notifsPerDay = result => {
   return { headers: ['date', users].flat(), items: results }
 }
 export default {
-  usagePerDay,
   notifsPerDay
 }
