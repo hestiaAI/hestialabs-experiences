ee7b28f7458642e57b3806c1815072bf653e1819
francoisquellec <francois.quellec@gmail.com>
2021-12-14 12:13:50 +0100

    fix sql query + use new sunburst + fix facebook.json to use sql pipeline

--- manifests/experiences/facebook/index.js
+++ manifests/experiences/facebook/index.js
@@ -1,7 +1,9 @@
 import postprocessors from './postprocessors'
 import customPipelines from './pipeline'
+import databaseBuilder from './database'
 
 export default {
   customPipelines,
+  databaseBuilder,
   postprocessors
 }
--- manifests/experiences/facebook/queries/off-facebook-activity-type-count.sql
+++ manifests/experiences/facebook/queries/off-facebook-activity-type-count.sql
@@ -1,33 +1,8 @@
-/* This one works but it is not what we want */
 SELECT name AS targetingType, type AS targetingValue, COUNT(*) as count
 FROM offFacebookActivityDatabase
-GROUP BY targetingType, targetingValue
-ORDER BY count DESC
-LIMIT 10;
-
-
-/* These ones are what we want but they do no work */
-/*
-SELECT name AS targetingType, type AS targetingValue, COUNT(*) as count
-FROM offFacebookActivityDatabase
-WHERE targetingType IN (
-  SELECT name AS targetingType
+WHERE targetingType in (SELECT name
   FROM offFacebookActivityDatabase
-  GROUP BY targetingType
+  GROUP BY name
   ORDER BY COUNT(*) DESC
-  LIMIT 10;
-)
-GROUP BY targetingType, targetingValue;
-*/
-
-/*
-SELECT name AS targetingType, type AS targetingValue, COUNT(*) as count
-FROM (
-  SELECT name AS targetingType, type AS targetingValue, COUNT(*) as countWorseApps
-  FROM offFacebookActivityDatabase
-  GROUP BY targetingType
-  ORDER BY countWorseApps DESC
-  LIMIT 10;
-)
-GROUP BY targetingType, targetingValue;
-*/
+  LIMIT 10)
+GROUP BY targetingType, targetingValue
