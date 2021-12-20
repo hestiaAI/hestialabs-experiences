195abee6d77a6ac432235999faee3120b9dbbad8
francoisquellec <francois.quellec@gmail.com>
2021-12-20 17:55:56 +0100

    text and typos fixes + ability to add xLabel to row graphs

--- components/chart/view/ChartViewOverviewNetflix.vue
+++ components/chart/view/ChartViewOverviewNetflix.vue
@@ -27,7 +27,7 @@
         <VRow dense>
           <VCol cols="8">
             <div :id="'hour-chart' + graphId">
-              <strong>Time of the day</strong>
+              <strong>Time of day</strong>
               <a class="reset" style="display: none">reset</a>
               <p class="filters ma-0">
                 <span>
@@ -39,7 +39,7 @@
           </VCol>
           <VCol cols="4">
             <div :id="'week-chart' + graphId">
-              <strong>Day of week</strong>
+              <strong>Day</strong>
               <a class="reset" style="display: none">reset</a>
               <p class="filters ma-0">
                 <span>
@@ -53,7 +53,7 @@
       </VCol>
       <VCol cols="4">
         <div :id="'content-chart' + graphId">
-          <strong>Top title</strong>
+          <strong>Most watched titles</strong>
           <a class="reset" style="display: none">reset</a>
           <p class="filters ma-0">
             <span>
@@ -67,7 +67,7 @@
     <VRow dense>
       <VCol cols="4">
         <div :id="'user-chart' + graphId">
-          <strong>Users</strong>
+          <strong>Profiles</strong>
           <a class="reset" style="display: none">reset</a>
           <p class="filters ma-0">
             <span>
@@ -107,8 +107,8 @@
         <span class="filter-count"></span>
         selected out of
         <span class="total-count"></span>
-        records |
-        <a class="reset">Reset All</a>
+        views |
+        <a class="resetAll">Reset All</a>
       </div>
     </VRow>
     <UnitFilterableTable :data="{ headers: header, items: results }" />
@@ -481,6 +481,7 @@ export default {
         .gap(1)
         .x(d3.scaleLinear().domain([0, 24]))
         .ordinalColors(colorPalette)
+        .yAxisLabel('Hours watched')
         .yAxis()
         .ticks(5)
       hourChart
@@ -545,13 +546,13 @@ export default {
         .groupAll(all)
         .html({
           some:
-            '<strong>%filter-count</strong> selected out of <strong>%total-count</strong> records' +
-            " | <a class='reset'>Reset All</a>",
-          all: 'All <strong>%total-count</strong> records selected. Please click on the graph to apply filters.'
+            '<strong>%filter-count</strong> selected out of <strong>%total-count</strong> views' +
+            " | <a class='resetAll'>Reset All</a>",
+          all: 'All <strong>%total-count</strong> views selected. Please click on the graph to apply filters.'
         })
         .on('pretransition', (chart, filter) => {
           this.results = monthDimension.top(all.value())
-          d3.select('#dc-data-count' + this.graphId + ' a.reset').on(
+          d3.select('#dc-data-count' + this.graphId + ' a.resetAll').on(
             'click',
             () => {
               dc.filterAll()
@@ -564,7 +565,7 @@ export default {
   }
 }
 </script>
-<style>
+<style scoped>
 @import 'assets/styles/dc.css';
 
 body {
@@ -576,17 +577,12 @@ body {
   fill: #22313f;
   font-weight: bold;
 }
-
 .range-chart > svg > g > g.axis.y {
   display: none;
 }
 
-.reset {
+a.reset {
   margin-left: 1rem;
-}
-
-.v-application a.reset {
-  color: rgb(85, 3, 30);
 }
 
 p.filters {
