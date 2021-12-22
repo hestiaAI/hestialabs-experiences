ed337e1042247be0e8f51c7141b6c4e6dca801c4
francoisquellec <francois.quellec@gmail.com>
2021-12-22 09:15:07 +0100

    init top row chart

---
+++ components/chart/view/ChartViewTopRow.vue
@@ -0,0 +1,42 @@
+<template>
+  <VContainer>
+    <VRow>
+      <VCol cols="12" md="12" class="text-center">
+        <p>
+          You have been targeted by <strong>{{ total }}</strong> ads between
+          15/04/2021 and 13/06/2021
+        </p>
+        <div :id="graphId"></div>
+      </VCol>
+    </VRow>
+  </VContainer>
+</template>
+
+<script>
+import mixin from './mixin'
+// import * as d3 from 'd3'
+
+export default {
+  mixins: [mixin],
+  props: {
+    dateFormat: {
+      type: String,
+      default: () => '%Y-%m-%d'
+    }
+  },
+  data() {
+    return {
+      total: 0
+    }
+  },
+  mounted() {
+    this.drawViz()
+  },
+  methods: {
+    drawViz() {
+      console.log(this.values)
+    }
+  }
+}
+</script>
+<style scoped></style>
