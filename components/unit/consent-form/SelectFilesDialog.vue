5d0b7ae1327ff4c64a7ab8ccf97de4c49a1240f7
Florian Singer <fsinger@mailbox.org>
2021-12-22 16:39:11 +0100

    style: minimalist file dialog

--- components/unit/consent-form/SelectFilesDialog.vue
+++ components/unit/consent-form/SelectFilesDialog.vue
@@ -1,10 +1,40 @@
 <template>
-  <VDialog v-model="show" width="80%" persistent scrollable>
+  <VDialog v-model="show" width="500" persistent scrollable>
     <VCard>
       <VCardTitle class="text-h5 grey lighten-2"> Select files </VCardTitle>
 
-      <VCardText style="height: 500px">
-        <UnitFileExplorer v-bind="{ fileManager, selectable: true }" />
+      <VCardText>
+        <VTextField
+          v-model="search"
+          label="Search for files"
+          placeholder="Enter part of a file name..."
+          clearable
+          hide-details
+          prepend-icon="$vuetify.icons.mdiMagnify"
+          class="my-4 pr-3"
+          style="max-width: 500px"
+          outlined
+          dense
+        />
+        <VTreeview
+          v-model="selectedFiles"
+          dense
+          open-on-click
+          return-object
+          transition
+          rounded
+          selectable
+          selected-color="primary"
+          :open.sync="openItems"
+          :search="search"
+          :items="treeItems"
+        >
+          <template #prepend="{ item }">
+            <VIcon>
+              {{ item.icon }}
+            </VIcon>
+          </template>
+        </VTreeview>
       </VCardText>
 
       <VDivider></VDivider>
@@ -32,7 +62,15 @@ export default {
       required: true
     }
   },
+  data() {
+    return {
+      search: ''
+    }
+  },
   computed: {
+    treeItems() {
+      return this.fileManager.getTreeItems()
+    },
     show: {
       get() {
         return this.value
@@ -41,8 +79,24 @@ export default {
         this.$emit('input', value)
       }
     },
+    selectedFiles: {
+      get() {
+        return this.$store.state.selectedFiles[this.key]
+      },
+      set(value) {
+        this.$store.commit('setSelectedFiles', { key: this.key, value })
+      }
+    },
     key() {
       return this.$route.params.key
+    }
+  },
+  watch: {
+    fileManager: {
+      immediate: true,
+      handler() {
+        this.setInitOpen()
+      }
     }
   },
   methods: {
@@ -54,6 +108,16 @@ export default {
       this.$store.commit('setSelectedFiles', { key: this.key, value: [] })
       this.show = false
       this.$emit('return', { clear: true })
+    },
+    setInitOpen() {
+      // If the root has a sequence of nodes with 1 children, pre-open them
+      const open = []
+      let tree = this.treeItems
+      while (tree.length === 1) {
+        open.push(tree[0])
+        tree = tree[0].children ?? []
+      }
+      this.openItems = open
     }
   }
 }
