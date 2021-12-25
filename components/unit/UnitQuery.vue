f4d82ae985c983a2751f27342027b732e190e8f4
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2021-12-25 22:59:50 +0100

    fix: consistent padding

--- components/unit/UnitQuery.vue
+++ components/unit/UnitQuery.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <VCard v-if="defaultViewElements" class="pa-2 my-6">
+    <VCard v-if="defaultViewElements" class="px-4 px-sm-8 py-4 my-6">
       <VCardTitle class="justify-center">{{
         defaultViewElements.title
       }}</VCardTitle>
