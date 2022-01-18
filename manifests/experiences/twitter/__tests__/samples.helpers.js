47bf5a679524f9e09fa51278c80b32a7d5ff0f7f
Florian Singer <fsinger@mailbox.org>
2022-01-18 17:44:28 +0100

    fix: twitter sql tests

--- manifests/experiences/twitter/__tests__/samples.helpers.js
+++ manifests/experiences/twitter/__tests__/samples.helpers.js
@@ -6,31 +6,31 @@ export const adImpressions = [
           impressions: [
             {
               deviceInfo: {
-                osType: 'Desktop'
+                osType: 'Android'
               },
               displayLocation: 'TimelineHome',
               promotedTweetInfo: {
-                tweetId: '1369584490276741122',
+                tweetId: '1381646278988292098',
                 tweetText:
-                  '30 Photos Taken In Dubai That Will Destroy Your Stereotypes..',
+                  '#TrustInTransformation: place your trust in a partner who can help you complete the future-forward transformation into a data-driven company. #CustomerCentricTransformation',
                 urls: [],
                 mediaUrls: []
               },
               advertiserInfo: {
-                advertiserName: 'Illumeably',
-                screenName: '@illumeably'
+                advertiserName: 'PwC Switzerland',
+                screenName: '@PwC_Switzerland'
               },
               matchedTargetingCriteria: [
                 {
-                  targetingType: 'Platforms',
-                  targetingValue: 'Desktop'
+                  targetingType: 'Locations',
+                  targetingValue: 'Switzerland'
                 },
                 {
-                  targetingType: 'Languages',
-                  targetingValue: 'English'
+                  targetingType: 'Age',
+                  targetingValue: '35 and up'
                 }
               ],
-              impressionTime: '2021-04-15 19:51:50'
+              impressionTime: '2021-04-15 19:43:25'
             }
           ]
         }
