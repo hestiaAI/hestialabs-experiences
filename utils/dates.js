e332a096cf8fbb9599960e4637a2cb686131f835
francoisquellec <francois.quellec@gmail.com>
2022-01-19 16:12:32 +0100

    improve date detection for tables and json

--- utils/dates.js
+++ utils/dates.js
@@ -1,5 +1,13 @@
+export function isValidDate(d) {
+  return d instanceof Date && !isNaN(d) && d.getFullYear() !== 1970
+}
 /**
  * Convert the given value to a formated date string depending on it's name
+ * If the name include date or time we assume that we can convert it with
+ * javascript Date object, then if the year founded after conversion is 1970,
+ * the starting date of unix timestamps, we assume that the timestamp is in
+ * seconds instead of milliseconds finally we return the value inchanged if
+ * we didnt succeed to convert it properly
  * @param {String} name the attribite name of the given value
  * @param {Any} value the value to convert
  * @returns the value transformed to local date string or the value inchanged if not possible
@@ -7,10 +15,10 @@
 export function toDateString(name, value) {
   const assumeDate = ['date', 'time'].find(d => name.toLowerCase().includes(d))
   if (assumeDate) {
-    return new Date(value).getFullYear() > 1980
-      ? new Date(value).toLocaleString()
-      : new Date(value * 1000).toLocaleString()
-  } else {
-    return value
+    let date = new Date(value)
+    if (isValidDate(date)) return date.toLocaleString()
+    date = new Date(value * 1000)
+    if (isValidDate(date)) return date.toLocaleString()
   }
+  return value
 }
