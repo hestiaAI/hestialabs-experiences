fb164fad68957ea6b2c2e0f1a52b710acd87b72e
francoisquellec <francois.quellec@gmail.com>
2022-01-27 15:05:23 +0100

    quote block component done

--- components/dataStoryFlows/base/QuoteBlock.vue
+++ components/dataStoryFlows/base/QuoteBlock.vue
@@ -1,38 +1,27 @@
 <template>
-  <VCard class="mx-auto" color="#26c6da" dark max-width="400">
-    <VCardTitle>
-      <VIcon large left> mdi-twitter </VIcon>
-      <span class="text-h6 font-weight-light">Twitter</span>
-    </VCardTitle>
-
-    <VCardText class="text-h5 font-weight-bold">
-      "Turns out semicolon-less style is easier and safer in TS because most
-      gotcha edge cases are type invalid as well."
+  <VCard class="mx-auto" flat>
+    <VCardText class="">
+      <VRow dense>
+        <VIcon large left color="primary">
+          $vuetify.icons.mdiFormatQuoteOpen
+        </VIcon>
+      </VRow>
+      <blockquote class="blockquote">
+        {{ text }}
+      </blockquote>
+      <VRow dense style="justify-content: end">
+        <VIcon large right color="primary">
+          $vuetify.icons.mdiFormatQuoteClose
+        </VIcon>
+      </VRow>
+      <div class="d-flex justify-space-between font-italic font-weight-thin">
+        <span>
+          <VIcon right color="primary"> $vuetify.icons.mdiMinus </VIcon>
+          {{ author }}
+        </span>
+        <a :href="link"> {{ link }}</a>
+      </div>
     </VCardText>
-
-    <VCardActions>
-      <VListItem class="grow">
-        <VListItemAvatar color="grey darken-3">
-          <VImg
-            class="elevation-6"
-            alt=""
-            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
-          ></VImg>
-        </VListItemAvatar>
-
-        <VListItemContent>
-          <VListItemTitle>Evan You</VListItemTitle>
-        </VListItemContent>
-
-        <VRow align="center" justify="end">
-          <VIcon class="mr-1"> mdi-heart </VIcon>
-          <span class="subheading mr-2">256</span>
-          <span class="mr-1">·</span>
-          <VIcon class="mr-1"> mdi-share-variant </VIcon>
-          <span class="subheading">45</span>
-        </VRow>
-      </VListItem>
-    </VCardActions>
   </VCard>
 </template>
 <script>
