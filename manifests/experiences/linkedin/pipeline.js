4888b9539561aa0c6de508d80d57035b91679971
fquellec <francois.quellec@gmail.com>
2022-03-23 17:44:26 +0100

    add new custom sub experience for Linkedin

--- manifests/experiences/linkedin/pipeline.js
+++ manifests/experiences/linkedin/pipeline.js
@@ -1,3 +1,18 @@
 import { genericDateViewer } from '~/manifests/generic-pipelines'
+import { setsEqual } from '~/utils/utils'
 
-export default { genericDateViewer }
+async function inference({ fileManager }) {
+  const tcFiles = await fileManager.getCsvItemsFromId('inference')
+
+  // Merge the files that have the same headers as the first file
+  const tcFile = tcFiles.reduce((prev, curr) => {
+    if (setsEqual(new Set(prev.headers), new Set(curr.headers))) {
+      return { headers: prev.headers, items: prev.items.concat(curr.items) }
+    } else {
+      return prev
+    }
+  })
+  return tcFile
+}
+
+export default { inference, genericDateViewer }
