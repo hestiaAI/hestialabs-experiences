21cbf959c0edc8374e4d009b1914f9c5027d6f56
fquellec <francois.quellec@gmail.com>
2022-04-08 16:34:49 +0200

    add heatmap calendar graph + refactor tinder experience

--- components/chart/view/ChartViewHeatMapCalendar.vue
+++ components/chart/view/ChartViewHeatMapCalendar.vue
@@ -1,128 +1,105 @@
 <template>
   <VContainer>
-    <VRow justify="center">
-      <VCol cols="12" md="9">
-        <div class="d-flex">
-          <div class="overline">{{ title }}</div>
-          <VSpacer></VSpacer>
-          <div>
-            <VRadioGroup v-model="aggType" row mandatory>
-              <VRadio label="Per hour" value="hour"></VRadio>
-              <VRadio label="Per day" value="day"></VRadio>
-            </VRadioGroup>
-          </div>
-        </div>
-        <svg class="graph" :viewBox="viewBox">
-          <g :transform="`translate(40.5,${cellSize * 1.5})`">
-            <g class="week-axis">
-              <text
-                v-for="(d, idx) in weekDays"
-                :key="`d_${idx}`"
-                x="-5"
-                :y="(idx + 0.5) * cellSize"
-                dy="0.35em"
-              >
-                {{ d }}
-              </text>
-            </g>
-
-            <g v-if="aggType === 'hour'" class="hour-axis">
-              <text y="-20" :x="(hours.length / 2 + 0.5) * cellSize">
-                Hours
-              </text>
-              <text
-                v-for="h in hours"
-                :key="`h_${h}`"
-                y="-5"
-                :x="(h + 0.5) * cellSize"
-              >
-                {{ h }}
-              </text>
-            </g>
-
-            <g v-if="aggType === 'hour'" class="calendar-hour">
-              <g v-for="(item, idx) in itemsPerHour" :key="idx">
-                <rect
-                  :width="cellSize - cellSpacing"
-                  :height="cellSize - cellSpacing"
-                  :x="item[0] * cellSize + 0.5"
-                  :y="item[1] * cellSize + 0.5"
-                  :fill="color(item[2])"
-                  :rx="borderRadius"
-                  :ry="borderRadius"
-                ></rect>
-                <title>{{ generateTitleHours(item) }}</title>
-              </g>
-            </g>
-
-            <g v-if="aggType === 'day'" class="calendar-hour">
-              <g
-                v-for="(year, idxYear) in itemsPerDay"
-                :key="'year_' + idxYear"
-              >
-                <g v-for="(day, idxDay) in year[1]" :key="'day_' + idxDay">
-                  <rect
-                    :width="cellSize - cellSpacing"
-                    :height="cellSize - cellSpacing"
-                    :x="xPos(year[0], day[0], day[1])"
-                    :y="idxYear * calendarHeight + day[2] * cellSize + 0.5"
-                    :fill="color(day[3])"
-                    :rx="borderRadius"
-                    :ry="borderRadius"
-                  ></rect>
-                  <title>
-                    {{ generateTitleDay(year[0], day[0], day[1], day[3]) }}
-                  </title>
-                </g>
-              </g>
-            </g>
-
-            <g class="legend">
-              <text
-                style="text-anchor: end"
-                :x="width - cellSize * legendNbItems * 3"
-                :y="height - cellSize * 2.5"
-                dy="-.20em"
-              >
-                {{ legendLabel }}
-              </text>
-              <g
-                v-for="(square, idx) in legendSquares"
-                :key="'legend_' + square"
-              >
-                <rect
-                  :width="(cellSize - cellSpacing) * 2"
-                  :height="(cellSize - cellSpacing) / 2"
-                  :x="
-                    width -
-                    cellSize * legendNbItems * 3 +
-                    idx * cellSize * 2 +
-                    cellSize
-                  "
-                  :y="height - cellSize * 3"
-                  :fill="color(square)"
-                  :rx="borderRadius"
-                  :ry="borderRadius"
-                ></rect>
-                <text
-                  style="text-anchor: middle"
-                  :x="
-                    width -
-                    cellSize * legendNbItems * 3 +
-                    idx * cellSize * 2 +
-                    2 * cellSize +
-                    0.5
-                  "
-                  :y="height - cellSize * 2"
-                >
-                  {{ square }}
-                </text>
-              </g>
+    <div class="overline mb-3">{{ title }}</div>
+    <svg class="graph" :viewBox="viewBox">
+      <g :transform="`translate(40.5,${cellSize * 1.5})`">
+        <g
+          v-for="(year, idxYear) in itemsPerDay"
+          :key="'week-axis' + idxYear"
+          class="week-axis"
+        >
+          <text
+            v-for="(d, idx) in weekDays"
+            :key="`d_${idx}`"
+            x="-5"
+            :y="idxYear * calendarHeight + (idx + 0.5) * cellSize"
+            dy="0.35em"
+          >
+            {{ d }}
+          </text>
+        </g>
+        <g
+          v-for="(item, idxYear) in monthsPerYear"
+          :key="'month-axis' + idxYear"
+          class="month-axis"
+        >
+          <text
+            v-for="(month, idx) in item.months"
+            :key="`month_${idx}`"
+            :x="xPosMonth(month)"
+            :y="idxYear * calendarHeight - 5"
+            dy="0.35em"
+          >
+            {{ formatMonth(month) }}
+          </text>
+        </g>
+        <g class="calendars">
+          <g v-for="(year, idxYear) in itemsPerDay" :key="'year_' + idxYear">
+            <text
+              :x="0.5"
+              :y="idxYear * calendarHeight - cellSize"
+              style="font-weight: bold; font-size: 12px"
+            >
+              {{ year[0] }}
+            </text>
+            <g v-for="(item, idxItem) in year[1]" :key="'day_' + idxItem">
+              <rect
+                :width="cellSize - cellSpacing"
+                :height="cellSize - cellSpacing"
+                :x="xPos(year[0], item[0], item[1])"
+                :y="idxYear * calendarHeight + item[2] * cellSize + 0.5"
+                :fill="color(item[3])"
+                :rx="borderRadius"
+                :ry="borderRadius"
+              ></rect>
+              <title>
+                {{ generateTitle(year[0], item[0], item[1], item[3]) }}
+              </title>
             </g>
           </g>
-        </svg>
-      </VCol>
-    </VRow>
+        </g>
+
+        <g class="legend">
+          <text
+            style="text-anchor: end"
+            :x="width - cellSize * legendNbItems * 3"
+            :y="height - cellSize * 2.5"
+            dy="-.20em"
+          >
+            {{ legendLabel }}
+          </text>
+          <g v-for="(square, idx) in legendSquares" :key="'legend_' + idx">
+            <rect
+              :width="(cellSize - cellSpacing) * 2"
+              :height="(cellSize - cellSpacing) / 2"
+              :x="
+                width -
+                cellSize * legendNbItems * 3 +
+                idx * cellSize * 2 +
+                cellSize
+              "
+              :y="height - cellSize * 3"
+              :fill="color(square)"
+              :rx="borderRadius"
+              :ry="borderRadius"
+            ></rect>
+            <text
+              style="text-anchor: middle"
+              :x="
+                width -
+                cellSize * legendNbItems * 3 +
+                idx * cellSize * 2 +
+                2 * cellSize +
+                0.5
+              "
+              :y="height - cellSize * 2"
+            >
+              {{ square }}
+            </text>
+          </g>
+        </g>
+      </g>
+    </svg>
   </VContainer>
 </template>
 
@@ -139,7 +116,7 @@ export default {
     },
     title: {
       type: String,
-      default: () => 'Title of the Graph'
+      default: () => ''
     },
     cellSize: {
       type: Number,
@@ -157,11 +134,6 @@ export default {
       type: String,
       default: () => ''
     },
-    dateAgg: {
-      type: String,
-      default: () => 'BOTH',
-      validator: v => ['BOTH', 'HOUR', 'DAY'].includes(v)
-    },
     borderRadius: {
       type: Number,
       default: () => 3
@@ -172,30 +144,25 @@ export default {
     },
     legendLabel: {
       type: String,
-      default: () => 'Number of test test'
+      default: () => ''
     }
   },
   data() {
     return {
       weekDays: ['Sun', 'Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat'],
-      hours: d3.range(24),
       formatMonth: d3.utcFormat('%b'),
-      aggType: null,
       colorPalette: d3.interpolateRdPu
     }
   },
   computed: {
     width() {
-      if (this.aggType === 'hour')
-        return this.cellSize * (this.hours.length + 3)
-      else return this.cellSize * (52 + 3)
+      return this.cellSize * (52 + 3)
     },
     calendarHeight() {
       return this.cellSize * (this.weekDays.length + 4)
     },
     height() {
-      if (this.aggType === 'hour') return this.calendarHeight
-      else return this.calendarHeight * this.itemsPerDay.length
+      return this.calendarHeight * this.itemsPerDay.length
     },
     viewBox() {
       return `0 0 ${this.width} ${this.height}`
@@ -212,17 +179,10 @@ export default {
         }
       })
     },
-    itemsPerHour() {
-      const hours = d3.flatRollup(
-        this.items,
-        v => d3.sum(v, d => d.value),
-        v => v.date.getHours(),
-        v => v.date.getDay()
-      )
-      return hours
-    },
     extent() {
-      return d3.extent(this.itemsPerHour, v => v[2])
+      return d3.extent(
+        this.itemsPerDay.flatMap(y => d3.extent(y[1], i => i[3]))
+      )
     },
     color() {
       return d3
@@ -245,6 +205,22 @@ export default {
         )
       ])
     },
+    monthsPerYear() {
+      const test = this.itemsPerDay.map(y => {
+        const dateExtent = d3.extent(y[1], i => {
+          console.log(y, i)
+          return new Date(y[0], i[0], i[1])
+        })
+        console.log(dateExtent)
+        return {
+          year: y[0],
+          months: d3.utcMonths(d3.utcMonth(dateExtent[0]), dateExtent[1])
+        }
+      })
+
+      console.log(test)
+      return test
+    },
     legendSquares() {
       const legendFormat = d3.format('.2')
       const step = this.extent[1] / (this.legendNbItems - 1)
@@ -253,21 +229,9 @@ export default {
         .map(n => (n ? legendFormat(n * step) : legendFormat(this.extent[0])))
     }
   },
-  mounted() {
-    console.log(this.extent[1])
-    console.log('Day', this.itemsPerDay)
-    console.log('hours ', this.itemsPerHour)
-  },
+  mounted() {},
   methods: {
-    generateTitleHours(item) {
-      return (
-        d3.timeFormat('%a at %H:00')(new Date(2000, 12, item[1], item[0])) +
-        ' - ' +
-        item[2] +
-        ' records'
-      )
-    },
-    generateTitleDay(year, month, day, value) {
+    generateTitle(year, month, day, value) {
       return (
         d3.timeFormat('%a %d %B, %Y')(new Date(year, month, day)) +
         ' - ' +
@@ -275,12 +239,14 @@ export default {
         ' records'
       )
     },
-    countDay(i) {
-      return (i + 6) % 7
-    },
     xPos(year, month, day) {
       const date = new Date(year, month, day)
       return d3.utcMonday.count(d3.utcYear(date), date) * this.cellSize + 0.5
+    },
+    xPosMonth(d) {
+      return (
+        d3.utcMonday.count(d3.utcYear(d), d3.utcMonday.ceil(d)) * this.cellSize
+      )
     },
     drawViz() {}
   }
