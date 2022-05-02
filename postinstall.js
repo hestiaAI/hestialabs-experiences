930e2062978b74cc527f4e29028e5d34fa2143bc
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2022-05-02 13:14:11 +0200

    build: add postinstall hook to install packages

---
+++ postinstall.js
@@ -0,0 +1,21 @@
+if (process.env.NETLIFY) {
+  const { spawnSync } = require('child_process')
+  const path = require('path')
+
+  const configName = process.env.CONFIG_NAME || 'config'
+
+  const { experiences } = require(path.resolve(
+    __dirname,
+    `config/${configName}.json`
+  ))
+
+  if (experiences) {
+    const packages = experiences.map(
+      packageNameAndTag => `@hestiaai/${packageNameAndTag}`
+    )
+    console.info(`
+  Installing the following packages from GitHub Package Registry:
+  ${packages.join('\n')}`)
+    spawnSync('npm', ['install', ...packages], { shell: true })
+  }
+}
