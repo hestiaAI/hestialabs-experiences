4c6eefd5a762fe6503c5a565fd1709ea0bdd4ee5
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2022-05-04 12:31:21 +0200

    fix:

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,7 +2,7 @@ export default {
   '**/*.{js,vue}': filenames =>
     // only lint changed files
     `npm run lint -- --fix ${filenames.join(' ')}`,
-  '{json,md}': filenames =>
+  '**/*.{json,md}': filenames =>
     // only format changed files
     `prettier --write ${filenames.join(' ')}`
 }
