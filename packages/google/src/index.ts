800cbd0dc782f9de0c061e8424dcb08a963b6d8a
fquellec <francois.quellec@gmail.com>
2022-05-09 22:53:52 +0200

    add guidance for google takeout data request

--- packages/google/src/index.ts
+++ packages/google/src/index.ts
@@ -4,6 +4,8 @@ import viewBlocks from './blocks'
 
 const options: ExperienceOptions = {
   dataPortal: 'https://takeout.google.com/settings/takeout',
+  dataPortalMessage:
+    '<strong>Important:</strong> For the experiment to work, please click on <strong>"deselect all"</strong> and then select <strong>"Location History"</strong> and <strong>"My Activity"</strong>. For the latter click on the button at the bottom left to select the <strong>JSON</strong> format.',
   files: {
     'my-activity':
       '**/*/+(MyActivity|My Activity|MonActivité|Mon Activité|OmatTapahtumat|Omat Tapahtumat).json'
