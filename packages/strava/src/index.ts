ee1c5c971bc82a54f951461557b3e2c6508148c9
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2022-05-09 14:18:41 +0200

    feat: add strava (https://github.com/hestiaAI/hestialabs-experiences/issues/669)

---
+++ packages/strava/src/index.ts
@@ -0,0 +1,14 @@
+import { Experience, ExperienceOptions } from '@/index'
+import icon from '@/icons/strava.png'
+import { genericViewers } from '@/pipelines/generic'
+
+const options: ExperienceOptions = {
+  dataPortal:
+    'https://support.strava.com/hc/en-us/articles/216918437-Exporting-your-Data-and-Bulk-Export#Bulk',
+  icon: icon,
+  slug: 'strava',
+  title: 'Strava',
+  viewBlocks: genericViewers
+}
+
+export default new Experience(options)
