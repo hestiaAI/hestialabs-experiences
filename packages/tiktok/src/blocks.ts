aafc68b76a006d4ff33cd2aa73e2ec97458b4d30
fquellec <francois.quellec@gmail.com>
2022-05-09 17:53:06 +0200

    fix sql call in tiktok

--- packages/tiktok/src/blocks.ts
+++ packages/tiktok/src/blocks.ts
@@ -1,10 +1,10 @@
 import type { ViewBlocks } from '@/types'
-
+import comments from './sql/comments.sql'
 const blocks: ViewBlocks = [
   {
     id: 'comments',
-    sql: 'comments',
-    files: ['comments'],
+    sql: comments,
+    files: ['userdata'],
     showTable: true,
     title: 'Comments',
     text: 'Comments recieved'
