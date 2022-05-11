ea67aea94922cfba354da90403bab9ff64cf185c
emmanuel@hestia.ai <emmanuel@hestia.ai>
2022-05-11 17:56:12 +0200

    Tiktok: added - fanlist - following - like list - login history - search history - share history - private messages

--- packages/tiktok/src/blocks.ts
+++ packages/tiktok/src/blocks.ts
@@ -1,13 +1,76 @@
 import type { ViewBlocks } from '@/types'
+import fanlist from './sql/fanlist.sql'
+import followinglist from './sql/followinglist.sql'
+import likelist from './sql/likelist.sql'
+import loginhistory from './sql/loginhistory.sql'
+import searchhistory from './sql/searchhistory.sql'
+import sharehistory from './sql/sharehistory.sql'
 import comments from './sql/comments.sql'
+import chathistory from './sql/chathistory.sql'
 const blocks: ViewBlocks = [
+  {
+    id: 'fanlist',
+    sql: fanlist,
+    files: ['userdata'],
+    showTable: true,
+    title: 'Fan list',
+    text: 'Users who follow you'
+  },
+  {
+    id: 'followinglist',
+    sql: followinglist,
+    files: ['userdata'],
+    showTable: true,
+    title: 'Following list',
+    text: 'Users whom you follow'
+  },
+  {
+    id: 'likelist',
+    sql: likelist,
+    files: ['userdata'],
+    showTable: true,
+    title: 'Like list',
+    text: 'Posts that you liked'
+  },
+  {
+    id: 'loginhistory',
+    sql: loginhistory,
+    files: ['userdata'],
+    showTable: true,
+    title: 'Login History',
+    text: 'When and where you logged in'
+  },
+  {
+    id: 'searchhistory',
+    sql: searchhistory,
+    files: ['userdata'],
+    showTable: true,
+    title: 'Search History',
+    text: 'What you searched'
+  },
+  {
+    id: 'sharehistory',
+    sql: sharehistory,
+    files: ['userdata'],
+    showTable: true,
+    title: 'Share History',
+    text: 'Items you shared'
+  },
   {
     id: 'comments',
     sql: comments,
     files: ['userdata'],
     showTable: true,
     title: 'Comments',
-    text: 'Comments recieved'
+    text: 'Comments received'
+  },
+  {
+    id: 'chathistory',
+    sql: chathistory,
+    files: ['userdata'],
+    showTable: true,
+    title: 'Chat History',
+    text: 'Private messages sent and received'
   }
 ]
 
--- packages/tiktok/src/database.ts
+++ packages/tiktok/src/database.ts
@@ -6,14 +6,184 @@ const { TEXT } = SQLType
 const config: DatabaseConfig = {
   tables: [
     {
+      name: 'TiktokFollowerList',
+      columns: [
+        ['date', TEXT],
+        ['username', TEXT]
+      ]
+    },
+    {
+      name: 'TiktokFollowingList',
+      columns: [
+        ['date', TEXT],
+        ['username', TEXT]
+      ]
+    },
+    {
+      name: 'LikeList',
+      columns: [
+        ['date', TEXT],
+        ['videolink', TEXT]
+      ]
+    },
+    {
+      name: 'LoginHistory',
+      columns: [
+        ['date', TEXT],
+        ['ip', TEXT],
+        ['devicemodel', TEXT],
+        ['devicesystem', TEXT],
+        ['networktype', TEXT],
+        ['carrier', TEXT]
+      ]
+    },
+    {
+      name: 'SearchHistory',
+      columns: [
+        ['date', TEXT],
+        ['searchterm', TEXT]
+      ]
+    },
+    {
+      name: 'ShareHistory',
+      columns: [
+        ['date', TEXT],
+        ['sharedcontent', TEXT],
+        ['link', TEXT],
+        ['method', TEXT]
+      ]
+    },
+    {
       name: 'TiktokComment',
       columns: [
         ['date', TEXT],
         ['comment', TEXT]
       ]
+    },
+    {
+      name: 'ChatHistory',
+      columns: [
+        ['date', TEXT],
+        ['sender', TEXT],
+        ['content', TEXT]
+      ]
     }
   ],
   getters: [
+    {
+      fileId: 'userdata',
+      path: '$.Activity.Follower List.FansList[*]',
+      table: 'TiktokFollowerList',
+      getters: [
+        {
+          column: 'date',
+          path: '$.Date'
+        },
+        {
+          column: 'username',
+          path: '$.UserName'
+        }
+      ]
+    },
+    {
+      fileId: 'userdata',
+      path: '$.Activity.Following List.Following[*]',
+      table: 'TiktokFollowingList',
+      getters: [
+        {
+          column: 'date',
+          path: '$.Date'
+        },
+        {
+          column: 'username',
+          path: '$.UserName'
+        }
+      ]
+    },
+    {
+      fileId: 'userdata',
+      path: '$.Activity.Like List.ItemFavoriteList[*]',
+      table: 'LikeList',
+      getters: [
+        {
+          column: 'date',
+          path: '$.Date'
+        },
+        {
+          column: 'videolink',
+          path: '$.VideoLink'
+        }
+      ]
+    },
+    {
+      fileId: 'userdata',
+      path: '$.Activity.Login History.LoginHistoryList[*]',
+      table: 'LoginHistory',
+      getters: [
+        {
+          column: 'date',
+          path: '$.Date'
+        },
+        {
+          column: 'ip',
+          path: '$.IP'
+        },
+        {
+          column: 'devicemodel',
+          path: '$.DeviceModel'
+        },
+        {
+          column: 'devicesystem',
+          path: '$.DeviceSystem'
+        },
+        {
+          column: 'networktype',
+          path: '$.NetworkType'
+        },
+        {
+          column: 'carrier',
+          path: '$.Carrier'
+        }
+      ]
+    },
+    {
+      fileId: 'userdata',
+      path: '$.Activity.Search History.SearchList[*]',
+      table: 'SearchHistory',
+      getters: [
+        {
+          column: 'date',
+          path: '$.Date'
+        },
+        {
+          column: 'searchterm',
+          path: '$.SearchTerm'
+        }
+      ]
+    },
+    {
+      fileId: 'userdata',
+      path: '$.Activity.Share History.ShareHistoryList[*]',
+      table: 'ShareHistory',
+      getters: [
+        {
+          column: 'date',
+          path: '$.Date'
+        },
+        {
+          column: 'sharedcontent',
+          path: '$.SharedContent'
+        },
+        {
+          column: 'link',
+          path: '$.Link'
+        },
+        {
+          column: 'method',
+          path: '$.Method'
+        }
+      ]
+    },
     {
       fileId: 'userdata',
       path: '$.Comment.Comments.CommentsList[*]',
@@ -26,6 +196,25 @@ const config: DatabaseConfig = {
         {
           column: 'comment',
           path: '$.Comment'
+        }
+      ]
+    }  ,
+    {
+      fileId: 'userdata',
+      path: '$.Direct Messages.Chat History.ChatHistory.*[*]',
+      table: 'ChatHistory',
+      getters: [
+        {
+          column: 'date',
+          path: '$.Date'
+        },
+        {
+          column: 'sender',
+          path: '$.From'
+        },
+        {
+          column: 'content',
+          path: '$.Content'
         }
       ]
     }
