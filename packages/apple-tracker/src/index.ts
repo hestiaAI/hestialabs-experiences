bfbc291d95edd0f5fa8ff37ea67512afb0d81a2d
fquellec <francois.quellec@gmail.com>
2022-06-08 15:49:44 +0200

    add AppleTracker experience

---
+++ packages/apple-tracker/src/index.ts
@@ -0,0 +1,34 @@
+import { Experience, ExperienceOptions } from '@/index'
+import icon from '@/icons/apple.png'
+import viewBlocks from './blocks'
+import databaseConfig from './database'
+import ndjsonToJson from './preprocessor'
+
+const options: ExperienceOptions = {
+  databaseConfig,
+  dataPortalHtml:
+    'In iOS 15.2, iPadOS 15.2, and watchOS 8.3 or later, users can view a privacy report of when your apps ' +
+    'accesses certain kinds of user data, like photos and contacts or sensitive device resources, ' +
+    'like the camera and microphone. You can also see when they contacts network domains, including websites ' +
+    'that you visit from within your apps. Examine the data that your apps contributes to this summary to make ' +
+    'sure that your app behaves as you expect. Once you receive it, analyze it here.<div style="margin: 1rem;"><ul style="display: inline-block;">' +
+    '<li align="left">Go to the Settings app then choose <b>Privacy > App Privacy Report</b>.</li>' +
+    '<li align="left">Enable app activity recording on your device by tapping <b>Turn On App Privacy Report</b></li>' +
+    '<li align="left">Run your apps for a while</li>' +
+    '<li align="left">Download the report by tapping the <b>Share button</b> on the App Privacy Report screen</li>' +
+    '<li align="left">Drop the downloaded file below</li></ul></div>',
+  dataSamples: ['App_Privacy_Report.ndjson'],
+  files: {
+    'tracker-control-ios': '**/*.ndjson'
+  },
+  icon: icon,
+  preprocessors: {
+    '**/*.ndjson': ndjsonToJson
+  },
+  slug: 'apple-tracker',
+  subtitle: 'Tracking data',
+  title: 'AppleTracker',
+  viewBlocks
+}
+
+export default new Experience(options)
