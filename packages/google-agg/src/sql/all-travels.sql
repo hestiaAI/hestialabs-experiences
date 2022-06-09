3ee6fe69d842ef32a6b6336e5578f7ba0f6e3cc1
wilderino <thomas.wilde@epfl.ch>
2022-06-09 16:36:00 +0200

    enhanced the public transport tab in the google agg experience

--- packages/google-agg/src/sql/all-travels.sql
+++ packages/google-agg/src/sql/all-travels.sql
@@ -1,7 +1,3 @@
-SELECT name_of_transport, COUNT() as number_of_participants
-FROM (
-SELECT DISTINCT SUBSTR(transitPath, 0, INSTR(transitPath, ':')) as name_of_transport, SUBSTR(FilePath, 0, INSTR(FilePath, '/')) as uniqueId
+SELECT DISTINCT startLatitude, startLongitude, endLatitude, endLongitude, transitPath, FilePath
 FROM Travels
-WHERE transitPath != 'null') a
-GROUP BY name_of_transport
-ORDER BY COUNT() desc
+WHERE transitPath != 'null'
