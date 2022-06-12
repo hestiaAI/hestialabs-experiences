959e617a92552187833bc4c8f6ca06bf4fd769d2
Stache <stache@stache.cat>
2022-06-12 17:49:33 +0200

    Part of #740

--- packages/tracker-control/src/blocks.ts
+++ packages/tracker-control/src/blocks.ts
@@ -8,7 +8,7 @@ const blocks: ViewBlocks = [
     files: ['tracker-control'],
     visualization: 'ChartViewTrackerControl.vue',
     title: 'Overview',
-    text: 'Monitor below all the tracking done on your smartphone applications. You can filter the results by selecting a time range or clicking on each graph.'
+    text: 'Monitor below all the tracking done on your smartphone applications. You can filter the results by selecting a time range, selecting certain apps in the dropdown, clicking on each graph, or using the table filters or search box. Any filtering you do will also limit what data is shared into the pool if you share this tab on the \'Share My Data\' tab.'
   }
 ]
 
