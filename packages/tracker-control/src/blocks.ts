67f1bd208d8d68e3be1b926b32f885b3c11f5219
Stache <stache@stache.cat>
2022-06-13 10:47:26 +0200

    Update texts

--- packages/tracker-control/src/blocks.ts
+++ packages/tracker-control/src/blocks.ts
@@ -6,9 +6,10 @@ const blocks: ViewBlocks = [
     id: 'trackerControl',
     customPipeline: customPipelineMergeCSV('tracker-control'),
     files: ['tracker-control'],
+    showTable: true,
     visualization: 'ChartViewTrackerControl.vue',
     title: 'Overview',
-    text: 'Monitor below all the tracking done on your smartphone applications. You can filter the results by selecting a time range, selecting certain apps in the dropdown, clicking on each graph, or using the table filters or search box. Any filtering you do will also limit what data is shared into the pool if you share this tab on the \'Share My Data\' tab.'
+    text: 'Monitor below all the tracking done on your smartphone applications. You can filter the results by selecting a time range, selecting certain apps in the dropdown, clicking on each graph, or using the table filters or search box.'
   }
 ]
 
