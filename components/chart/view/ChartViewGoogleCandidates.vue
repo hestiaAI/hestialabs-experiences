3d9738f4d0d1f40cfd810ebc88a86e3cf583485d
wilderino <thomas.wilde@epfl.ch>
2022-06-16 17:51:05 +0200

    added the config back for agg

---
+++ components/chart/view/ChartViewGoogleCandidates.vue
@@ -0,0 +1,98 @@
+<template>
+  <VContainer v-if="values.length > 0">
+    <VRow>
+      <VCol cols="12">
+        <p v-if="total === 0" class="text-subtitle-2">
+          No records were found in your file(s).
+        </p>
+        <p v-else class="text-subtitle-2">
+          This map shows the other candidate places proposed by Google linked to
+          the location that Google think you visited:
+        </p>
+      </VCol>
+    </VRow>
+    <template v-if="total > 0">
+      <VRow>
+        <VCol cols="12">
+          <UnitIframe src="/kepler" :args="keplerArgs" />
+        </VCol>
+        <p v-if="config.consent">
+          Please use the search box and filters below to change what is shown on
+          the map.<br />
+          Any filtering you do will also limit what data is shared into the pool
+          if you share this tab on the 'Share My Data' tab.
+        </p>
+      </VRow>
+      <VRow>
+        <VCol cols="12">
+          <UnitFilterableTable
+            v-bind="{ headers, items: results }"
+            @current-items="onTableFilter"
+          />
+        </VCol>
+      </VRow>
+    </template>
+  </VContainer>
+</template>
+<script>
+import { mapState } from 'vuex'
+import mixin from './mixin'
+import keplerConfig from './kepler_config_candidate.js'
+export default {
+  mixins: [mixin],
+  data() {
+    return {
+      filteredRows: []
+    }
+  },
+  computed: {
+    ...mapState(['config']),
+    results() {
+      return this.values.map(v => {
+        return {
+          ...v,
+          winnerLatitude: v.winnerLatitude * 1e-7,
+          winnerLongitude: v.winnerLongitude * 1e-7,
+          winnerSemanticType:
+            v.winnerSemanticType === 'undefined' ? null : v.winnerSemanticType,
+          loserLatitude: v.loserLatitude * 1e-7,
+          loserLongitude: v.loserLongitude * 1e-7,
+          loserSemanticType:
+            v.loserSemanticType === 'undefined' ? null : v.loserSemanticType,
+          loserAddress: v.loserAddress === 'undefined' ? null : v.loserAddress,
+          winnerName: v.winnerName === 'undefined' ? null : v.winnerName,
+          loserName: v.loserName === 'undefined' ? null : v.loserName
+        }
+      })
+    },
+    total() {
+      return this.results.length
+    },
+    filtered() {
+      return this.filteredRows.length
+    },
+    keplerData() {
+      return {
+        fields: this.headers.map(h => {
+          return {
+            name: h
+          }
+        }),
+        rows: this.filteredRows.map(r => this.headers.map(h => r[h]))
+      }
+    },
+    keplerArgs() {
+      return {
+        keplerData: this.keplerData,
+        config: keplerConfig
+      }
+    }
+  },
+  methods: {
+    drawViz() {},
+    onTableFilter(newItems) {
+      this.filteredRows = newItems
+    }
+  }
+}
+</script>
