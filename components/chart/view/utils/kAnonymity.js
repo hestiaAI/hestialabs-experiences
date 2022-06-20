79b2fb8369332b6b8f6605a1ea710ada11c9cb5d
wilderino <thomas.wilde@epfl.ch>
2022-06-20 11:36:02 +0200

    enhanced the kanonymity

--- components/chart/view/utils/kAnonymity.js
+++ components/chart/view/utils/kAnonymity.js
@@ -21,39 +21,24 @@ function filterOut(arr, k) {
   return arr.filter(x => x.k <= k)
 }
 
-export function kAnonymityFilter(values, groupKey, otherKeys) {
+export function kAnonymityFilter(values, groupKeys, otherKeys) {
   const table = []
-  if (groupKey.length === 1) {
-    const grouped = _.groupBy(values, groupKey[0])
-    const keys = Object.keys(grouped)
-    for (let i = 0; i < keys.length; i++) {
-      const x = keys[i]
-      const elem = {}
-      elem[groupKey[0]] = x
-      const kMin = findK(grouped[x].map(v => v.k))
-      const rest = filterOut(grouped[x], kMin)
-      for (let j = 0; j < otherKeys.length; j++) {
-        elem[otherKeys[j]] = avg(rest.map(x => x[otherKeys[j]]))
-      }
-      elem.k = kMin
-      table.push(elem)
+  const grouped = _.groupBy(values, n => groupKeys.map(x => n[x]))
+  const keys = Object.keys(grouped)
+  for (let i = 0; i < keys.length; i++) {
+    const x = keys[i]
+    const elem = {}
+    const splitted = x.split(',')
+    for (let j = 0; j < groupKeys.length; j++) {
+      elem[groupKeys[j]] = splitted[j]
     }
-  } else if (groupKey.length === 2) {
-    const grouped = _.groupBy(values, n => [n[groupKey[0]], n[groupKey[1]]])
-    const keys = Object.keys(grouped)
-    for (let i = 0; i < keys.length; i++) {
-      const x = keys[i]
-      const elem = {}
-      elem[groupKey[0]] = x.substring(0, x.indexOf(','))
-      elem[groupKey[1]] = x.substring(x.indexOf(',') + 1)
-      const kMin = findK(grouped[x].map(v => v.k))
-      elem.k = kMin
-      const rest = filterOut(grouped[x], kMin)
-      for (let j = 0; j < otherKeys.length; j++) {
-        elem[otherKeys[j]] = avg(rest.map(x => x[otherKeys[j]]))
-      }
-      table.push(elem)
+    const kMin = findK(grouped[x].map(v => v.k))
+    const rest = filterOut(grouped[x], kMin)
+    for (let j = 0; j < otherKeys.length; j++) {
+      elem[otherKeys[j]] = avg(rest.map(x => x[otherKeys[j]]))
     }
+    elem.k = kMin
+    table.push(elem)
   }
   const res = table.filter(x => x.k > 0)
   return res
