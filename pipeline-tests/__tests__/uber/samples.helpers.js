7334b4eee2adfc7ddcd2e03116af043ae3873c6f
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2022-06-29 09:49:17 +0200

    test(uber): fix headers of raw csv

--- pipeline-tests/__tests__/uber/samples.helpers.js
+++ pipeline-tests/__tests__/uber/samples.helpers.js
@@ -1,4 +1,4 @@
-export const testTripsData = `City,Product Type,tripOrOrderStatus,requestTime,beginTripTime,beginTripLat,beginTripLng,beginTripAddress,dropoffTime,dropoffLat,dropoffLng,dropoffAddress,distanceMiles,fareAmount,fareCurrency
+export const testTripsData = `City,Product Type,Trip or Order Status,Request Time,Begin Trip Time,Begin Trip Lat,Begin Trip Lng,Begin Trip Address,Dropoff Time,Dropoff Lat,Dropoff Lng,Dropoff Address,Distance (miles),Fare Amount,Fare Currency
 42,uberX,COMPLETED,2020-07-19 14:23:45 +0000 UTC,2020-07-19 14:52:45 +0000 UTC,42.2342,3.14159,"Sesame Street, UK",2020-07-19 15:28:45 +0000 UTC,23.42,2.71828,"23 Plouf Street, Smurfbridge H7 2FU, UK",6.66,23,GBP
 42,uberX,COMPLETED,2020-07-21 09:28:49 +0000 UTC,2020-07-21 09:41:49 +0000 UTC,42.2342,3.14159,"Sesame Street, UK",2020-07-21 10:07:49 +0000 UTC,23.42,2.71828,"23 Plouf Street, Smurfbridge H7 2FU, UK",6.66,23,GBP
 `
