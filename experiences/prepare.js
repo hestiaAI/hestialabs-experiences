5112b9a2d928842f68f4a09b00b1be6dc00aa3de
andreaskundig <andreas.kuendig@gmx.ch>
2022-07-13 18:47:25 +0200

    chore: remove a comment

--- experiences/prepare.js
+++ experiences/prepare.js
@@ -1,5 +1,4 @@
 if (process.env.NODE_ENV !== 'production') {
-  // const out = require('child_process').execSync('husky install')
   const out = require('child_process').execSync('cd ../ && husky install ./experiences/.husky')
   console.info(out.toString('utf8'))
 }
