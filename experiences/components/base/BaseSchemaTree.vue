5e9359048c01b35a64902ab83f9d207ad62e41db
fquellec <francois.quellec@gmail.com>
2022-07-22 16:14:59 +0200

    clean

---
+++ experiences/components/base/BaseSchemaTree.vue
@@ -0,0 +1,77 @@
+<template>
+  <VCard
+    v-if="validSchema"
+    outlined
+    hover
+    class="ma-3"
+    :dark="selected"
+    @click="clickOnCard"
+    @click.stop=""
+  >
+    <VCardTitle>
+      {{ schema.fieldName || schema.fileName }}
+      <VSpacer />
+      <VChip
+        outlined
+        color="primary"
+        :disabled="schema['@type'] === null"
+        link
+        :href="schema['@type']"
+        target="_blank"
+      >
+        {{ schema['@type'] || 'Unknow' }}
+      </VChip>
+    </VCardTitle>
+    <VCardSubtitle>
+      {{ schema['description'] }}
+    </VCardSubtitle>
+    <VCardText>
+      <BaseSchemaTree
+        v-for="child in schema.contains"
+        :key="child.absolutePath"
+        ref="childs"
+        :schema="child"
+      />
+    </VCardText>
+  </VCard>
+</template>
+
+<script>
+
+import { isEmpty } from 'lodash-es'
+
+export default {
+  name: 'BaseSchemaTree',
+  props: {
+    schema: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      selected: false
+    }
+  },
+  computed: {
+    validSchema() {
+      return !isEmpty(this.schema)
+    },
+    isLeaf() {
+      return !this.schema.contains?.length
+    }
+  },
+  methods: {
+    clickOnCard() {
+      this.selected = !this.selected
+      if (this.isLeaf) {
+        if (this.selected) {
+          this.$store.commit('selectPath', this.schema.absolutePath)
+        } else {
+          this.$store.commit('unselectPath', this.schema.absolutePath)
+        }
+      }
+    }
+  }
+}
+</script>
--- experiences/components/unit/file-explorer/viewer/UnitFileExplorerViewerSchema.vue
+++ experiences/components/unit/file-explorer/viewer/UnitFileExplorerViewerSchema.vue
@@ -30,7 +30,7 @@
 <script>
 import { mapState } from 'vuex'
 import mixin from './mixin'
-import BaseSchemaTree from '@/components/base/schemaTree/BaseSchemaTree.vue'
+import BaseSchemaTree from '@/components/base/BaseSchemaTree.vue'
 import BaseProgressCircular from '@/components/base/BaseProgressCircular.vue'
 export default {
   name: 'UnitFileExplorerViewerRaw',
