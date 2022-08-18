cbe7206b65a8e07bb4ce43bc67120e8e9f40e615
wilderino <thomas.wilde@epfl.ch>
2022-08-18 13:35:19 +0200

    added the crow distance and crow ratio

--- packages/packages/uber-driver/src/postprocessor.ts
+++ packages/packages/uber-driver/src/postprocessor.ts
@@ -1,15 +1,73 @@
 import { PostprocessorFunction } from '@/types'
 
+function computeCrowDist(
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+) {
+  const R = 6371 // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1) // deg2rad below
+  const dLon = deg2rad(lon2 - lon1)
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2)
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+  const d = R * c // Distance in km
+  return d
+}
+
+function deg2rad(deg: number) {
+  return deg * (Math.PI / 180)
+}
+
+function computeCrowRatio(crow_dist: number, dist: number) {
+  return dist / crow_dist
+}
+
 export const driverTripsPostProcessor: PostprocessorFunction = result => {
   const items = result?.items || []
   if ('distance' in items[0]) {
     const results = items.map(
       // we want to remove the trip point as they are unreadable
-      ({ distance, dropoffTime, begintripPoint, dropoffPoint, ...rest }) => {
+      ({
+        distance,
+        dropoffTime,
+        begintripPoint,
+        dropoffPoint,
+        begintripLatitude,
+        begintripLongitude,
+        dropoffLatitude,
+        dropoffLongitude,
+        ...rest
+      }) => {
         return {
+          begintripLatitude: begintripLatitude,
+          begintripLongitude: begintripLongitude,
+          dropoffLatitude: dropoffLatitude,
+          dropoffLongitude: dropoffLongitude,
+          distanceKm: (distance * 1.609344).toFixed(3),
+          crowDistance: computeCrowDist(
+            begintripLatitude,
+            begintripLongitude,
+            dropoffLatitude,
+            dropoffLongitude
+          ).toFixed(3),
+          crowRatio: computeCrowRatio(
+            computeCrowDist(
+              begintripLatitude,
+              begintripLongitude,
+              dropoffLatitude,
+              dropoffLongitude
+            ),
+            distance * 1.609344
+          ).toFixed(3),
+
           dropoffTime: dropoffTime.replace(/\n/g, ''),
-          ...rest,
-          distanceKm: (distance * 1.609344).toFixed(3)
+          ...rest
         }
       }
     )
