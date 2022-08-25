682e27bf7c3c89d41885ad6646feb639fa510f10
andreaskundig <andreas.kuendig@gmx.ch>
2022-08-25 14:47:08 +0200

    rebuilt

---
+++ experiences/dist/_nuxt/ce38a3d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[71],{1598:function(t,e,n){"use strict";n.r(e),e.default=n.p+"assets/data/Twitter_model.50d4ba2.json"}}]);
--- experiences/dist/_nuxt/f69ce32.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[72],{1596:function(e,n,o){"use strict";o.r(n),n.default=o.p+"assets/data/people.63d2ff4.json"}}]);
