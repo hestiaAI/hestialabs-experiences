41e0c4040902124dad82e7df0c6affb4d5ab7e3f
andreaskundig <andreas.kuendig@gmx.ch>
2022-09-28 11:29:55 +0200

    replace twitter with twitterb

--- packages/packages/twitterb/src/sql/ads-per-advertiser.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  advertiserName,
-  DATE(t.time) AS date_,
-  COUNT(*) AS count_
-FROM
-  TwitterAd t
-GROUP BY
-  advertiserName;
--- packages/packages/twitterb/src/sql/targeting-criteria-all-advertisers.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  targetingType,
-  targetingValue,
-  COUNT(*) AS count_
-FROM
-  TwitterCriterion
-GROUP BY
-  targetingType,
-  targetingValue;
