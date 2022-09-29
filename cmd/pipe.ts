1df4b0db5a467df8db15957f5112009e0ce0ea26
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2022-09-29 11:16:48 +0200

    chore(cmd): progress

--- cmd/pipe.ts
+++ cmd/pipe.ts
@@ -1,3 +1,24 @@
-const [outFile, ...inputFiles] = process.argv.slice(2)
+import FileManager, { NodeFile } from '../experiences/utils/file-manager'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
 
-console.log(outFile, inputFiles)
+import experiences from '@hestiaai/hestialabs'
+
+console.log(experiences)
+
+// const fileManager = new FileManager(
+//   this.preprocessors,
+//   fileManagerWorkers,
+//   this.files,
+//   this.keepOnlyFiles
+// )
+// try {
+//   await fileManager.init(uppyFiles)
+//   this.$store.commit('setFileManager', fileManager)
+//   if (dbConfig) {
+//     // create database
+//     const db = await DBMS.createDB(dbConfig)
+//     // generate database records via the file manager
+//     const records = await DBMS.generateRecords(fileManager, dbConfig)
+//     // insert the records into the database
+//     DBMS.insertRecords(db, records)
