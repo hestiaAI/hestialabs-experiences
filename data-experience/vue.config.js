759a340470e53bedca9e824d365a69bd57e0cf8f
andreaskundig <andreas.kuendig@gmx.ch>
2022-10-14 14:08:55 +0200

    working on npm serve

--- data-experience/vue.config.js
+++ data-experience/vue.config.js
@@ -5,7 +5,7 @@ const path = require('path')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './data-experience/dist/',
+  publicPath: './data-experience/',
   configureWebpack: {
     resolve: {
       alias: {
