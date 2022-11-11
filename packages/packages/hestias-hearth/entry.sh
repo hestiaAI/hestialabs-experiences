59c06bdc2f5164b6f641aabe6a7a8a08f4dfc801
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2022-11-11 21:51:28 +0100

    fix: ERR_MODULE_NOT_FOUND

--- packages/packages/hestias-hearth/entry.sh
+++ packages/packages/hestias-hearth/entry.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # https://stackoverflow.com/questions/68111434/how-to-run-node-js-cli-with-experimental-specifier-resolution-node
-/usr/bin/env node --experimental-specifier-resolution=node ./index.mjs "$@"
+/usr/bin/env node --experimental-specifier-resolution=node $(dirname $0)/index.mjs "$@"
