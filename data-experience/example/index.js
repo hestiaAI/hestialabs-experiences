78f1598f2b2466765f9cd36c9eba64108a64ae87
andreaskundig <andreas.kuendig@gmx.ch>
2022-11-14 18:52:15 +0100

    v0.3.0 cdn example not yet working

--- data-experience/example/index.js
+++ data-experience/example/index.js
@@ -1,10 +1,18 @@
+import twitter from "https://cdn.jsdelivr.net/npm/@hestia.ai/tinder@1.0.0/dist/index.mjs"
+
 Vue.use(Vuex)
+
 const store = new Vuex.Store({})
 Vue.use(DataExperience.default, { store })
 
 new Vue({
   el: '#app',
   i18n: DataExperience.i18n,
-  vuetify: new Vuetify(DataExperience.vuetifyOpts)
+  vuetify: new Vuetify(DataExperience.vuetifyOpts),
+  store,
+  data: {
+    experienceConfig: twitter.options,
+    siteConfig: {}
+  }
 })
 
