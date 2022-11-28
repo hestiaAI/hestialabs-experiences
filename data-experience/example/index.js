b07c1a330751ae47e6ca63de19af7f5ca70a0d64
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2022-11-28 13:21:17 +0100

    fix(data-experience): update example

--- data-experience/example/index.js
+++ data-experience/example/index.js
@@ -1,14 +1,20 @@
-import twitter from "https://cdn.jsdelivr.net/npm/@hestia.ai/twitter@1.0.0/dist/index.mjs"
+import twitter from 'https://cdn.jsdelivr.net/npm/@hestia.ai/twitter@1.x/dist/index.mjs'
 
 Vue.use(Vuex)
+Vue.use(VueI18n)
+
+const i18n = new VueI18n(DataExperience.i18nOpts)
+
+const vuetify = new Vuetify(DataExperience.vuetifyOpts(i18n))
 
 const store = new Vuex.Store({})
+
 Vue.use(DataExperience.default, { store })
 
 new Vue({
   el: '#app',
-  i18n: DataExperience.i18n,
-  vuetify: new Vuetify(DataExperience.vuetifyOpts),
+  i18n,
+  vuetify,
   store,
   data: {
     experienceConfig: twitter.options,
