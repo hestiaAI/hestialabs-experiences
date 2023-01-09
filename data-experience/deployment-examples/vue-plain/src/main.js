aaa5c784f1ff0dfef23d9f946be416e5943c68b5
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2023-01-09 08:58:27 +0100

    feat(data-experience): Vue deployment example WIP

---
+++ data-experience/deployment-examples/vue-plain/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+import App from './App.vue'
+import store from './store'
+import DataExperience, { vuetifyOpts, i18nOpts } from '@hestia.ai/data-experience'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(VueI18n)
+const i18n = new VueI18n(i18nOpts)
+console.log(DataExperience, i18nOpts, vuetifyOpts)
+Vue.use(Vuetify)
+const vuetify = new Vuetify(vuetifyOpts(i18n))
+
+Vue.use(DataExperience.default, { store })
+
+new Vue({
+  i18n,
+  vuetify,
+  store,
+  render: h => h(App)
+}).$mount('#app')
