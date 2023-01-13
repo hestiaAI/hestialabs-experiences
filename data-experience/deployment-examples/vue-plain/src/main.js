d1d835c0fbed2b113277116e30c5d8019135479c
Valentin Oliver Loftsson <valentin.loftsson@gmail.com>
2023-01-13 15:17:28 +0100

    feat(data-experience): update deployment examples

--- data-experience/deployment-examples/vue-plain/src/main.js
+++ data-experience/deployment-examples/vue-plain/src/main.js
@@ -3,19 +3,20 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-// import DataExperience, { i18nOpts, vuetifyOpts } from '@hestia.ai/data-experience'
+import DataExperience, { i18nOpts, vuetifyOpts } from '@hestia.ai/data-experience'
 // import '@hestia.ai/data-experience/dist/DataExperience.css'
 
 import store from './store'
 import App from './App.vue'
+console.log(DataExperience, i18nOpts, vuetifyOpts)
 
 Vue.use(VueI18n)
-const i18n = new VueI18n(DataExperience.i18nOpts)
+const i18n = new VueI18n(i18nOpts)
 
 Vue.use(Vuetify)
-const vuetify = new Vuetify(DataExperience.vuetifyOpts(i18n))
+const vuetify = new Vuetify(vuetifyOpts(i18n))
 
-Vue.use(DataExperience.default, { store })
+Vue.use(DataExperience, { store })
 
 new Vue({
   store,
