0b3286d0696d7e8bb2a3c45c4065a79c3640e28a
andreaskundig <andreas.kuendig@gmx.ch>
2023-02-20 20:39:31 +0100

    ViewBlock .configureI18n .buildProps

--- data-experience/deployment-examples/html-plain/block.js
+++ data-experience/deployment-examples/html-plain/block.js
@@ -5,31 +5,22 @@ const data = await dataResp.json()
 const messagesFrResp = await fetch('./fr.json')
 const messagesFr = await messagesFrResp.json()
 
-const viewBlock = experience.config.viewBlocks[0]
-
-const locale = 'fr'
-const expName = experience.title
-messagesFr[expName] = experience.config.messages[locale]
-const viewBlockTranslationPrefix = `${expName}.viewBlocks.${viewBlock.id}`
+const ViewBlock = DataExperience.ViewBlock
+Vue.use(DataExperience.ViewBlock)
 
 Vue.use(VueI18n)
 const i18n = new VueI18n({
   ...DataExperience.i18nOpts,
-  locale,
-  messages: { fr: messagesFr }
+  locale: 'fr'
 })
+ViewBlock.configureI18n(i18n, experience, messagesFr)
+
+const props = ViewBlock.buildProps(data, experience, 'overview')
 
 const vuetify = new Vuetify(DataExperience.vuetifyOpts(i18n))
-
-Vue.use(DataExperience.ViewBlock)
-
 new Vue({
   el: '#app',
   i18n,
   vuetify,
-  data: {
-    data,
-    viewBlockTranslationPrefix,
-    viewBlock
-  }
+  data: { props }
 })
--- data-experience/src/main.js
+++ data-experience/src/main.js
@@ -6,42 +6,41 @@ import './assets/dc-custom.css'
 
 import { MODULE_NAME } from '@/utils/store-helper'
 import UnitPipelineViewBlock from './components/unit/UnitPipelineViewBlock'
+import {
+  kViewBlockPrefix, mergeMessagesIntoI18n, experienceNameFromTitle,
+  nestExperienceLocaleMessages, injectTranslationHelpersIntoVue
+}
+  from '@/utils/i18n-utils'
 export { default as vuetifyOpts } from './plugins/vuetify'
 export { default as i18nOpts } from './plugins/i18n'
 export { default as chartView } from './components/chart/ChartView.vue'
 export { default as chartViewBar } from './components/chart/view/ChartViewBar.vue'
 export { default as chartViewGenericMap } from './components/chart/view/ChartViewGenericMap.vue'
 
-function injectTranslationHelpers(Vue) {
-  try {
-    Vue.prototype.$tev = function(key, valueFallback) {
-      // tev -> Translation Exists (else) Value fallback
-      return this.$te(key) ? this.$t(key) : valueFallback
-    }
-  } catch (err) {
-    throw new Error('Conflict: $tev function is already injected in the host app, please change its name.')
-  }
-  try {
-    Vue.prototype.$tet = function(key, keyFallback) {
-      // tet -> Translation Exists (else) Translate fallback
-      return this.$te(key) ? this.$t(key) : this.$t(keyFallback)
-    }
-  } catch (err) {
-    throw new Error('Conflict: $tet function is already injected in the host app, please change its name.')
-  }
-  try {
-    Vue.prototype.$days = function() {
-      return ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map(v => this.$t(`dayOfWeek.${v}`))
-    }
-  } catch (err) {
-    throw new Error('Conflict: $days function is already injected in the host app, please change its name.')
-  }
-}
-
 export const ViewBlock = {
   install(Vue, options) {
-    injectTranslationHelpers(Vue)
+    injectTranslationHelpersIntoVue(Vue, options?.ignoreConflicts)
     Vue.component('ViewBlock', UnitPipelineViewBlock)
+  },
+  configureI18n(i18n, experience, appLocaleMessages) {
+    const { config: { title, messages } } = experience
+    const { locale } = i18n
+    const experienceName = experienceNameFromTitle(title)
+    const expLocaleMessages =
+          nestExperienceLocaleMessages(experienceName, messages[locale])
+    const messagesList = [expLocaleMessages, appLocaleMessages]
+    mergeMessagesIntoI18n(i18n, locale, messagesList)
+  },
+  buildProps(data, experience, viewBlockId) {
+    const { config: { title, viewBlocks } } = experience
+    const experienceName = experienceNameFromTitle(title)
+    const viewBlock = viewBlocks.find(vb => vb.id === viewBlockId)
+    const prefix = kViewBlockPrefix(experienceName, viewBlockId)
+    return {
+      data,
+      viewBlockTranslationPrefix: prefix,
+      ...viewBlock
+    }
   }
 }
 
@@ -50,7 +49,7 @@ export const DataExperience = {
     if (!options || !options.store) {
       throw new Error('Please initialise plugin with a Vuex store.')
     }
-    injectTranslationHelpers(Vue)
+    injectTranslationHelpersIntoVue(Vue)
     options.store.registerModule(MODULE_NAME, store)
     Vue.component('TheDataExperience', TheDataExperience)
   }
