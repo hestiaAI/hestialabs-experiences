a6233f6d38b6008ce3c6c04509f48c288dbc4fee
andreaskundig <andreas.kuendig@gmx.ch>
2023-02-20 14:32:11 +0100

    working viewblock deployment example

--- data-experience/deployment-examples/html-plain/block.js
+++ data-experience/deployment-examples/html-plain/block.js
@@ -1,39 +1,43 @@
 /* eslint-disable no-undef */
 import experience from 'https://cdn.jsdelivr.net/npm/@hestia.ai/twitter/dist/index.mjs'
-// import data from './twitter-overview.json' assert {type: 'json'}
 const dataResp = await fetch('./twitter-overview.json')
 const data = await dataResp.json()
-
+const messagesFrResp = await fetch('./fr.json')
+const messagesFr = await messagesFrResp.json()
 const viewBlock = experience.config.viewBlocks[0]
+
+const locale = 'fr'
+const expName = 'my-exp'
+const experienceMessages = experience.config.messages[locale]
 
 Vue.use(Vuex)
 Vue.use(VueI18n)
 
-const i18n = new VueI18n(DataExperience.i18nOpts)
+const i18n = new VueI18n({
+  ...DataExperience.i18nOpts,
+  locale,
+  messages: { fr: { [expName]: experienceMessages } }
+  // messages: { fr: messagesFr },
+})
+i18n.mergeLocaleMessage(locale, messagesFr)
 
 const vuetify = new Vuetify(DataExperience.vuetifyOpts(i18n))
 
 const store = new Vuex.Store({})
 
-// TODO: remove use of store in UnitFilterableTable
-// It comes from:
-// import kViewBlockMixin from '@/mixins/k-view-block'
-
 // needed for installing the store and setting globals like $tev
 Vue.use(DataExperience.DataExperience, { store })
 Vue.component('unit-pipeline-view-block', DataExperience.UnitPipelineViewBlock)
-// console.log('store', store)
-// store.commit('setExperienceConfig', experience.config)
 
 new Vue({
   el: '#app',
   i18n,
   vuetify,
-  store,
+  // store,
   data: {
     data,
     missingFiles: [],
-    translationKeyPrefix: 'dodo',
+    translationKeyPrefix: `${expName}.viewBlocks.overview`,
     viewBlock
   }
 })
