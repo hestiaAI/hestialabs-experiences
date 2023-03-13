86ea5e3c657de7dc3a66bffc103f8de66f70f624
andreaskundig <andreas.kuendig@gmx.ch>
2023-03-13 12:00:10 +0100

    comment unused code, untested

--- data-experience/deployment-examples/wordpress-plugin/data-experience-iframe.php
+++ data-experience/deployment-examples/wordpress-plugin/data-experience-iframe.php
@@ -23,32 +23,32 @@ function func_load_scripts() {
   // wp_register_script('daex_setup', plugin_dir_url( __FILE__ ).'data-experience-setup.js', 'daex_dataexp', true );
   wp_register_script('daex_iframe', plugin_dir_url( __FILE__ ).'iframe.js');
 }
-function add_module_to_script( $tag, $handle, $src ) {
-  global $dataexp_setup_js;
-  $modules = array('daex_vuetify', 'daex_setup');
-  if (in_array($handle, $modules)) {
-      $tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
-  }
-  if ($handle == 'daex_dataexp') {
-      // $tag = $tag.'<script type="module">console.log("Hello from filter, world!", `'.$dataexp_setup_js.'`);</script>';
-      $tag = $tag.'<script type="module">'.$dataexp_setup_js.'</script>';
-  }
-  return $tag;
-}
+// function add_module_to_script( $tag, $handle, $src ) {
+//   global $dataexp_setup_js;
+//   $modules = array('daex_vuetify', 'daex_setup');
+//   if (in_array($handle, $modules)) {
+//       $tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
+//   }
+//   if ($handle == 'daex_dataexp') {
+//       // $tag = $tag.'<script type="module">console.log("Hello from filter, world!", `'.$dataexp_setup_js.'`);</script>';
+//       $tag = $tag.'<script type="module">'.$dataexp_setup_js.'</script>';
+//   }
+//   return $tag;
+// }
 
 add_action('wp_enqueue_scripts', 'func_load_scripts');
-add_filter( 'script_loader_tag', 'add_module_to_script', 10, 3 );
+// add_filter( 'script_loader_tag', 'add_module_to_script', 10, 3 );
 
-function func_load_styles() {
-  global $dataexp_version;
-  wp_register_style('daex_font_roboto', 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900');
-  wp_register_style('daex_font_icons', 'https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css');
-  wp_register_style('daex_style_vuetify', 'https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css');
-  wp_register_style('daex_style_uppy', 'https://releases.transloadit.com/uppy/v3.3.0/uppy.min.css');
-  wp_register_style('daex_style_dataexp', 'https://cdn.jsdelivr.net/npm/@hestia.ai/data-experience@'
-                      .$dataexp_version.'/dist/DataExperience.css');
-}
-add_action('wp_enqueue_scripts', 'func_load_styles');
+// function func_load_styles() {
+//   global $dataexp_version;
+//   wp_register_style('daex_font_roboto', 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900');
+//   wp_register_style('daex_font_icons', 'https://cdn.jsdelivr.net/npm/@mdi/font@6.x/css/materialdesignicons.min.css');
+//   wp_register_style('daex_style_vuetify', 'https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css');
+//   wp_register_style('daex_style_uppy', 'https://releases.transloadit.com/uppy/v3.3.0/uppy.min.css');
+//   wp_register_style('daex_style_dataexp', 'https://cdn.jsdelivr.net/npm/@hestia.ai/data-experience@'
+//                       .$dataexp_version.'/dist/DataExperience.css');
+// }
+// add_action('wp_enqueue_scripts', 'func_load_styles');
 
 //Add shortcode
 // https://developer.wordpress.org/plugins/shortcodes/shortcodes-with-parameters/#complete-example
@@ -79,26 +79,26 @@ function func_data_experience($atts){
   <iframe id="data-experience-iframe" width="100%" height="100%"></iframe>
 END;
 
-  $dataexp_setup_js = <<<END
-  import experience from 'https://cdn.jsdelivr.net/npm/@hestia.ai/$experience/dist/index.mjs'
-  Vue.use(Vuex)
-  Vue.use(VueI18n)
-  const i18n = new VueI18n(DataExperience.i18nOpts)
-  const vuetify = new Vuetify(DataExperience.vuetifyOpts(i18n))
-  const store = new Vuex.Store({})
-  Vue.use(DataExperience.DataExperience, { store })
-  const mapboxToken = 'pk.eyJ1IjoiYW5kcmVhc2t1bmRpZyIsImEiOiJja3ZxcnlmNXc2ZzUwMnFva2F2a3Q1azU5In0.NrvCU8OKlkwJOVFOgZzTzA'
-  new Vue({
-    el: '#app',
-    i18n,
-    vuetify,
-    store,
-    data: {
-      experienceConfig: experience.config,
-      siteConfig: { mapboxToken }
-    }
-  })
-END;
+//   $dataexp_setup_js = <<<END
+//   import experience from 'https://cdn.jsdelivr.net/npm/@hestia.ai/$experience/dist/index.mjs'
+//   Vue.use(Vuex)
+//   Vue.use(VueI18n)
+//   const i18n = new VueI18n(DataExperience.i18nOpts)
+//   const vuetify = new Vuetify(DataExperience.vuetifyOpts(i18n))
+//   const store = new Vuex.Store({})
+//   Vue.use(DataExperience.DataExperience, { store })
+//   const mapboxToken = 'pk.eyJ1IjoiYW5kcmVhc2t1bmRpZyIsImEiOiJja3ZxcnlmNXc2ZzUwMnFva2F2a3Q1azU5In0.NrvCU8OKlkwJOVFOgZzTzA'
+//   new Vue({
+//     el: '#app',
+//     i18n,
+//     vuetify,
+//     store,
+//     data: {
+//       experienceConfig: experience.config,
+//       siteConfig: { mapboxToken }
+//     }
+//   })
+// END;
 
   wp_enqueue_script('daex_iframe');
   // wp_enqueue_script('daex_vuejs');
