7ae7ad200b95434aa8e7631744376ff006979cde
fquellec <francois.quellec@gmail.com>
2023-06-19 09:24:38 +0200

    remove html message from google exp

--- packages/packages/experiences/google/src/index.ts
+++ packages/packages/experiences/google/src/index.ts
@@ -9,9 +9,6 @@ import dataSample from '@/data-samples/google-takeout.zip'
 const options: ExperienceOptions = {
   databaseConfig,
   dataPortal: 'https://takeout.google.com/settings/takeout',
-  dataPortalMessage:
-    '<strong>Important:</strong> For the experiment to work, please click on <strong>"deselect all"</strong> and then select the following: <strong>"Location History"</strong><div>' +
-    'Once done you can click the blue "Next Step" button at the bottom of the page. This should queue your download',
   files: {
     LOCATION_HISTORY: '**/*.json'
   },
