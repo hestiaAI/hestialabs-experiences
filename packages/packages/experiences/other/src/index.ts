b37e31fd63cb6459999bee9b790509ad521228cb
andreaskundig <andreas.kuendig@gmx.ch>
2023-07-12 17:54:26 +0200

    distinguish loader and viewer options in Experience constructor

--- packages/packages/experiences/other/src/index.ts
+++ packages/packages/experiences/other/src/index.ts
@@ -10,4 +10,4 @@ const options: ExperienceOptions = {
   viewBlocks: []
 }
 
-export default new Experience(options, undefined, packageJSON, import.meta.url)
+export default new Experience(options, options, packageJSON, import.meta.url)
