968e1edfd3d01ffc06e32e239754b63e31465fb3
andreaskundig <andreas.kuendig@gmx.ch>
2023-07-12 16:12:30 +0200

    viewer.json

--- packages/packages/experiences/instagram/src/index.ts
+++ packages/packages/experiences/instagram/src/index.ts
@@ -1,18 +1,22 @@
 import packageJSON from '../package.json'
 import { Experience, ExperienceOptions } from '@/index'
-import icon from '@/icons/instagram.png'
+// import icon from '@/icons/instagram.png'
 import viewBlocks from './blocks'
 import databaseConfig from './database'
 import messages from './messages.json'
 import dataSample from '@/data-samples/instagramV2.zip'
 import preprocessor from '../../facebook/src/preprocessor'
+import viewerConfig from './viewer.json'
+console.log('vc', viewerConfig)
 
 const options: ExperienceOptions = {
+  // version: 2,
+  //
+  // version 3 load external viewer.json during build
+  // version 4 load external viewer.json during runtime (user)
+  // problem: aggregation bubbles depend on presentation of view tabs
+  // loader
   databaseConfig,
-  dataPortal: 'https://help.instagram.com/181231772500920',
-  dataPortalMessage:
-    '<strong>Important:</strong> To make the experience work, please request your data in <strong>JSON</strong> format.',
-  dataSamples: [dataSample],
   files: {
     messages: '**/messages/inbox/**/message_*.json',
     followers: '**/followers.json',
@@ -35,11 +39,17 @@ const options: ExperienceOptions = {
   preprocessors: {
     '**/*.json': preprocessor
   },
-  hideFileExplorer: false,
-  icon,
-  messages,
+  // TODO goes into viewer.json
   title: 'Instagram',
-  viewBlocks
+  dataPortal: 'https://help.instagram.com/181231772500920',
+  dataPortalMessage:
+    '<strong>Important:</strong> To make the experience work, please request your data in <strong>JSON</strong> format.',
+  dataSamples: [dataSample],
+  // icon,
+  icon: 'https://raw.githubusercontent.com/hestiaAI/hestialabs-experiences/a29ee41dca8d2d8aaf2e377e6422e820f66db1bd/packages/lib/icons/instagram.png',
+  hideFileExplorer: false,
+  viewBlocks,
+  messages
 }
 
 export default new Experience(options, packageJSON, import.meta.url)
