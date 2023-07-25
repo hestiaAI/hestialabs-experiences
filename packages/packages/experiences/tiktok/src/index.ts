8f17c31ac82a00637fb473bac51f3a008d3001af
andreaskundig <andreas.kuendig@gmx.ch>
2023-07-25 17:50:31 +0200

    migrate tiktok to viewer.json

--- packages/packages/experiences/tiktok/src/index.ts
+++ packages/packages/experiences/tiktok/src/index.ts
@@ -1,28 +1,19 @@
 import packageJSON from '../package.json'
-import { Experience, ExperienceOptions } from '@/index'
-import { theEyeballs } from '@/collaborators/index'
-import icon from '@/icons/tiktok.png'
-import viewBlocks from './blocks'
+import { Experience, LoaderOptions, ViewerOptions } from '@/index'
+import viewerOptions from './tiktok-viewer.json'
 import databaseConfig from './database'
-import messages from './messages.json'
-import dataSample from '@/data-samples/tiktok.zip'
 
-const options: ExperienceOptions = {
-  collaborator: theEyeballs,
+const loaderOptions: LoaderOptions = {
+  viewerVersion: 1,
   databaseConfig,
-  dataPortal:
-    'https://support.tiktok.com/en/account-and-privacy/personalized-ads-and-data/requesting-your-data',
-  dataPortalMessage:
-    '<strong>Important:</strong> To make the experiment work, please request your data in <strong>JSON</strong> format.',
-  dataSamples: [dataSample],
   files: {
     userdata: '**/user_data.json'
-  },
-  hideFileExplorer: false,
-  messages,
-  icon,
-  title: 'TikTok',
-  viewBlocks
+  }
 }
 
-export default new Experience(options, options, packageJSON, import.meta.url)
+export default new Experience(
+  loaderOptions,
+  viewerOptions as ViewerOptions,
+  packageJSON,
+  import.meta.url
+)
