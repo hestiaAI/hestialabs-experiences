a9ecfcd378ecdb2f14668ec09e265cf4b6d250b8
andreaskundig <andreas.kuendig@gmx.ch>
2023-07-25 10:45:39 +0200

    add keepOnlyfiles back to explorer loader

--- packages/packages/experiences/explorer/src/index.ts
+++ packages/packages/experiences/explorer/src/index.ts
@@ -3,7 +3,8 @@ import { Experience, LoaderOptions, ViewerOptions } from '@/index'
 import viewerOptions from './explorer-viewer.json'
 
 const loaderOptions: LoaderOptions = {
-  viewerVersion: 1
+  viewerVersion: 1,
+  keepOnlyFiles: false
 }
 
 export default new Experience(
