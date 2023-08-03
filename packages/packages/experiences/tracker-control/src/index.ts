2f8d23f1be8ff635516a7e3e9a81a4c784b470d3
andreaskundig <andreas.kuendig@gmx.ch>
2023-08-03 16:41:59 +0200

    tracker-control-viewer.json

--- packages/packages/experiences/tracker-control/src/index.ts
+++ packages/packages/experiences/tracker-control/src/index.ts
@@ -1,25 +1,19 @@
 import packageJSON from '../package.json'
-import { Experience, ExperienceOptions } from '@/index'
-import icon from '@/icons/tracker-control.png'
-import viewBlocks from './blocks'
-import messages from './messages.json'
-import dataSample from '@/data-samples/tracker-control.csv'
+import viewerFunctions from './viewer-functions'
+import { Experience, LoaderOptions, ViewerOptions } from '@/index'
+import viewerOptions from './tracker-control-viewer.json'
 
-const options: ExperienceOptions = {
-  dataPortalHtml:
-    'Install and enable TrackerControl to monitor the behaviour of the apps on your Android phone, using ' +
-    '<a href="https://github.com/hestiaAI/data-catalog/blob/main/workshop/install-and-enable-trackercontrol.md" target="_blank" rel="noreferrer noopener" >these install instructions</a>. ' +
-    'Once you have collected some data, export it (<a href="https://github.com/hestiaAI/data-catalog/blob/main/workshop/how-to-export-data-from-trackercontrol.md" target="_blank" rel="noreferrer noopener" >export instructions here</a>)' +
-    ' and analyze it here.',
-  dataSamples: [dataSample],
+const loaderOptions: LoaderOptions = {
+  viewerVersion: 1,
   files: {
     'tracker-control': '**/*.csv'
-  },
-  icon,
-  messages,
-  subtitle: 'Tracking data',
-  title: 'TrackerControl',
-  viewBlocks
+  }
 }
 
-export default new Experience(options, options, packageJSON, import.meta.url)
+export default new Experience(
+  loaderOptions,
+  viewerOptions as ViewerOptions,
+  packageJSON,
+  import.meta.url,
+  viewerFunctions
+)
