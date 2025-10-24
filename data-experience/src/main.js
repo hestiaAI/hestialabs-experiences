50ee0bbf1ee9207fe0b984056bf9d2899e6af1a9
Fabian Studer <fabian.studer@students.fhnw.ch>
2025-10-24 08:16:43 +0200

    initial commit of Digipower Experience

--- data-experience/src/main.js
+++ data-experience/src/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import TheDataExperience from './components/TheDataExperience.vue'
 import CodeEditorComponent from './components/misc/CodeEditor.vue'
 import ChartViewComponent from './components/chart/ChartView.vue'
@@ -69,3 +70,76 @@ export const DataExperience = {
     Vue.component('TheDataExperience', TheDataExperience)
   }
 }
+=======
+import TheDataExperience from './components/TheDataExperience.vue'
+import CodeEditorComponent from './components/misc/CodeEditor.vue'
+import ChartViewComponent from './components/chart/ChartView.vue'
+
+import store from './store'
+import './assets/dc.css'
+import './assets/dc-custom.css'
+
+import { MODULE_NAME } from '@/utils/store-helper'
+import UnitPipelineViewBlock from './components/unit/UnitPipelineViewBlock'
+import {
+  kViewBlockPrefix, mergeMessagesIntoI18n, experienceNameFromTitle,
+  nestExperienceLocaleMessages, injectTranslationHelpersIntoVue
+}
+  from '@/utils/i18n-utils'
+export { default as vuetifyOpts } from './plugins/vuetify'
+export { default as i18nOpts } from './plugins/i18n'
+export { default as chartViewBar } from './components/chart/view/ChartViewBar.vue'
+export { default as chartViewGenericMap } from './components/chart/view/ChartViewGenericMap.vue'
+export { hashFile, encryptFile } from './utils/encryption'
+export { default as BubbleAPI } from './utils/bubble-api'
+
+export const CodeEditor = {
+  install(Vue, options) {
+    Vue.component('CodeEditor', CodeEditorComponent)
+  }
+}
+
+export const ChartView = {
+  install(Vue, options) {
+    Vue.component('ChartView', ChartViewComponent)
+  }
+}
+export const ViewBlock = {
+  install(Vue, options) {
+    injectTranslationHelpersIntoVue(Vue, options?.ignoreConflicts)
+    Vue.component('ViewBlock', UnitPipelineViewBlock)
+  },
+  configureI18n(i18n, experience, appLocaleMessages, vuetifyMessages) {
+    const { config: { title, messages } } = experience
+    const { locale } = i18n
+    appLocaleMessages.$vuetify = vuetifyMessages
+    const experienceName = experienceNameFromTitle(title)
+    const expLocaleMessages =
+          nestExperienceLocaleMessages(experienceName, messages[locale])
+    const messagesList = [expLocaleMessages, appLocaleMessages]
+    mergeMessagesIntoI18n(i18n, locale, messagesList)
+  },
+  buildProps(data, experience, viewBlockId) {
+    const { config: { title, viewBlocks } } = experience
+    const experienceName = experienceNameFromTitle(title)
+    const viewBlock = viewBlocks.find(vb => vb.id === viewBlockId)
+    const prefix = kViewBlockPrefix(experienceName, viewBlockId)
+    return {
+      data,
+      viewBlockTranslationPrefix: prefix,
+      ...viewBlock
+    }
+  }
+}
+
+export const DataExperience = {
+  install(Vue, options) {
+    if (!options || !options.store) {
+      throw new Error('Please initialise plugin with a Vuex store.')
+    }
+    injectTranslationHelpersIntoVue(Vue)
+    options.store.registerModule(MODULE_NAME, store)
+    Vue.component('TheDataExperience', TheDataExperience)
+  }
+}
+>>>>>>> 515b1db0 (initial commit of Digipower Experience)
--- data-experience/vue.config.js
+++ data-experience/vue.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import webpack from 'webpack'
 import { defineConfig } from '@vue/cli-service'
 import path from 'path'
@@ -79,3 +80,112 @@ export default defineConfig({
     }
   }
 })
+=======
+import webpack from 'webpack'
+import { defineConfig } from '@vue/cli-service'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { createRequire } from 'module'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const require = createRequire(import.meta.url)
+
+const isDev = process.env.NODE_ENV === 'development'
+
+export default defineConfig({
+  publicPath: isDev ? '/' : './',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, 'src'),
+        '@': path.resolve(__dirname, 'src'),
+        // https://github.com/micromatch/picomatch/pull/23
+        picomatch: 'picomatch-browser',
+        '@hestia.ai/apple-tracker': path.resolve(__dirname, '../packages/packages/experiences/apple-tracker'),
+        '@hestia.ai/apple-tracker-agg': path.resolve(__dirname, '../packages/packages/experiences/apple-tracker-agg'),
+        '@hestia.ai/chatgpt': path.resolve(__dirname, '../packages/packages/experiences/chatgpt'),
+        '@hestia.ai/explorer': path.resolve(__dirname, '../packages/packages/experiences/explorer'),
+        '@hestia.ai/facebook': path.resolve(__dirname, '../packages/packages/experiences/facebook'),
+        '@hestia.ai/fitbit': path.resolve(__dirname, '../packages/packages/experiences/fitbit'),
+        '@hestia.ai/google': path.resolve(__dirname, '../packages/packages/experiences/google'),
+        '@hestia.ai/google-agg': path.resolve(__dirname, '../packages/packages/experiences/google-agg'),
+        '@hestia.ai/her': path.resolve(__dirname, '../packages/packages/experiences/her'),
+        '@hestia.ai/her-tinder-agg': path.resolve(__dirname, '../packages/packages/experiences/her-tinder-agg'),
+        '@hestia.ai/instagram': path.resolve(__dirname, '../packages/packages/experiences/instagram'),
+        '@hestia.ai/linkedin': path.resolve(__dirname, '../packages/packages/experiences/linkedin'),
+        '@hestia.ai/linkedin-agg': path.resolve(__dirname, '../packages/packages/experiences/linkedin-agg'),
+        '@hestia.ai/netflix': path.resolve(__dirname, '../packages/packages/experiences/netflix'),
+        '@hestia.ai/strava': path.resolve(__dirname, '../packages/packages/experiences/strava'),
+        '@hestia.ai/tiktok': path.resolve(__dirname, '../packages/packages/experiences/tiktok'),
+        '@hestia.ai/tinder': path.resolve(__dirname, '../packages/packages/experiences/tinder'),
+        '@hestia.ai/tracker-control': path.resolve(__dirname, '../packages/packages/experiences/tracker-control'),
+        '@hestia.ai/tracker-control-agg': path.resolve(__dirname, '../packages/packages/experiences/tracker-control-agg'),
+        '@hestia.ai/twitter': path.resolve(__dirname, '../packages/packages/experiences/twitter'),
+        '@hestia.ai/twitter-agg': path.resolve(__dirname, '../packages/packages/experiences/twitter-agg'),
+        '@hestia.ai/uber': path.resolve(__dirname, '../packages/packages/experiences/uber'),
+        '@hestia.ai/uber-driver': path.resolve(__dirname, '../packages/packages/experiences/uber-driver'),
+        '@hestia.ai/wolt': path.resolve(__dirname, '../packages/packages/experiences/wolt'),
+        '@hestia.ai/youtube': path.resolve(__dirname, '../packages/packages/experiences/youtube'),
+        '@hestia.ai/database-template': path.resolve(__dirname, '../packages/packages/experiences/database-template')
+      },
+      fallback: {
+        fs: false,
+        crypto: false, // require.resolve("crypto-browserify") to add polyfill
+        path: require.resolve('path-browserify'),
+        util: require.resolve('util/')
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.worker\.js$/,
+          use: [
+            {
+              loader: 'worker-loader',
+              options: {
+                inline: 'fallback',
+                filename: '[name].[contenthash:7].js'
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        // for init() in sql.js
+        'process.release': {}
+      })
+    ],
+    experiments: {
+      topLevelAwait: true
+    }
+  },
+  // Fix issue when project path has a special symbol like '(' or ')'.
+  // Bug of copy-webpack-plugin or fast-glob, because cli will add the absolute path of the index.html
+  // in the public folder to the ignore list, if the path has a special symbol, copy-webpack-plugin will
+  // not match the path and will not ignore it. Therefore index.html will emit twice.
+  // source: https://github.com/vuejs/vue-cli/issues/7043#issuecomment-1069791347
+  chainWebpack: (config) => {
+    if (isDev) {
+      config.plugin('copy').tap((args) => {
+        const UNESCAPED_GLOB_SYMBOLS_RE = /(\\?)([()*?[\]{|}]|^!|[!+@](?=\())/g
+        const publicDir = path.resolve(process.VUE_CLI_SERVICE.context, 'public').replace(/\\/g, '/')
+        const escapePublicDir = publicDir.replace(UNESCAPED_GLOB_SYMBOLS_RE, '\\$2')
+        args[0].patterns[0].globOptions.ignore = args[0].patterns[0].globOptions.ignore.map(i => i.replace(publicDir, escapePublicDir))
+        return args
+      })
+    }
+  },
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true,
+      enableBridge: false
+    }
+  }
+})
+>>>>>>> 515b1db0 (initial commit of Digipower Experience)
