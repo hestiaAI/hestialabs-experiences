77caa30882eb3c9e68bb0d7a59a5e63a3959c420
Fabian Studer <fabian.studer@students.fhnw.ch>
2025-10-24 13:46:43 +0200

    added own experience as package

---
+++ packages/packages/experiences/uber-eats/src/index.ts
@@ -0,0 +1,20 @@
+import packageJSON from '../package.json'
+import { Experience, LoaderOptions, ViewerOptions } from '@/index'
+import viewerOptions from './uber-eats.json'
+import databaseConfig from './database'
+
+const loaderOptions: LoaderOptions = {
+  viewerVersion: 1,
+  databaseConfig,
+  files: {
+    LOCATION_HISTORY: '**/*.json'
+  },
+  keepOnlyFiles: true
+}
+
+export default new Experience(
+  loaderOptions,
+  viewerOptions as ViewerOptions,
+  packageJSON,
+  import.meta.url
+)
