d8a7c9c4315e9cd9ca7d32dc7799b9d68f05bb6d
Fabian Studer <fabian.studer@students.fhnw.ch>
2026-01-07 15:11:47 +0100

    test commit

--- data-experience/playwright.config.js
+++ data-experience/playwright.config.js
@@ -2,7 +2,7 @@
 import { devices } from '@playwright/test'
 
 /**
- * Read environment variables from file.
+ * Read environment variables from file
  * https://github.com/motdotla/dotenv
  */
 // import dotenv from 'dotenv'
